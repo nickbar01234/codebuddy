e5c2d0a4823cc48c7b78224a485a379375f55345
nickbar01234 <nickbar01234@gmail.com>
2024-05-31 14:58:41 +0700

    Mount app

--- extension/src/App.tsx
+++ extension/src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const App = () => {
-  return <div className="h-32 w-32 bg-red-500">Code Buddy</div>;
+  return <div className="cb-h-full cb-w-[200px] cb-bg-red-500">Code Buddy</div>;
 };
 
 export default App;
--- extension/src/index.tsx
+++ extension/src/index.tsx
@@ -1,17 +1,26 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import "./style/index.css";
 import App from "./App";
+import { waitForElement } from "./utils";
+import "./style/index.css";
 
-const root = document.createElement("div");
-root.id = "CodeBuddy";
+const TIME_OUT = 2000; // ms
+const LEETCODE_ROOT_ID = "#qd-content";
+const EXTENSION_ID = "CodeBuddy";
 
-document.body.appendChild(root);
-
-const entry = createRoot(root);
-
-entry.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+waitForElement(LEETCODE_ROOT_ID, TIME_OUT)
+  .then((leetCodeNode) => {
+    const extensionRoot = document.createElement("div");
+    extensionRoot.id = EXTENSION_ID;
+    leetCodeNode.insertAdjacentElement("afterend", extensionRoot);
+    createRoot(extensionRoot).render(
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    );
+  })
+  .catch((_reason) =>
+    console.error(
+      `Unable to mount ${EXTENSION_ID} within ${TIME_OUT} - most likely due to LeetCode changing HTML page`
+    )
+  );
