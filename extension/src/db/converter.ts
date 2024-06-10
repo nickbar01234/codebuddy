1f59f3ffccfa6321d88cdd1a21113418491d1148
nickbar01234 <nickbar01234@gmail.com>
2024-06-10 18:01:38 +0700

    Add converter

---
+++ extension/src/db/converter.ts
@@ -0,0 +1,50 @@
+import {
+  FirestoreDataConverter,
+  QueryDocumentSnapshot,
+  SnapshotOptions,
+} from "firebase/firestore";
+
+export interface Room {
+  usernames: string[];
+}
+
+export interface PeerConnection {
+  username?: string;
+
+  offer?: RTCSessionDescriptionInit;
+  offerCandidates: RTCIceCandidate[];
+
+  answer?: RTCSessionDescriptionInit;
+  answerCandidates: RTCIceCandidate[];
+}
+
+export const roomConverter: FirestoreDataConverter<Room, Room> = {
+  toFirestore: (data: Room) => data,
+
+  fromFirestore: (
+    snapshot: QueryDocumentSnapshot,
+    options: SnapshotOptions
+  ): Room => {
+    const data = snapshot.data(options)! ?? {};
+    return {
+      ...data,
+      usernames: data.usernames ?? [],
+    };
+  },
+};
+
+export const peerConnectionConverter: FirestoreDataConverter<
+  PeerConnection,
+  PeerConnection
+> = {
+  toFirestore: (data: PeerConnection) => data,
+
+  fromFirestore: (snapshot, options) => {
+    const data = snapshot.data(options) ?? {};
+    return {
+      ...data,
+      offerCandidates: data.offerCandidates ?? [],
+      answerCandidates: data.answerCandidates ?? [],
+    };
+  },
+};
