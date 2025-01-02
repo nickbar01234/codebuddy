de536712a7856bf75be6cd7ac080a617b8efabc2
Nick Doan <nickbar01234@gmail.com>
2025-01-02 12:37:26 -0500

    Add split editor (#58)

---
+++ extension/src/constants/index.ts
@@ -0,0 +1,11 @@
+import { ExtensionStorage } from "@cb/types";
+
+export const CodeBuddyPreference: ExtensionStorage = {
+  appPreference: {
+    width: 300 /* px */,
+    isCollapsed: false,
+  },
+  codePreference: {
+    height: 500,
+  },
+};
