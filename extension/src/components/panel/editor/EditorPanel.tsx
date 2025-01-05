bf0c34af0fbf8977ac3a9377ff3d0d8c5469b2d0
HUNG NGO <hungngo1607@gmail.com>
2025-01-05 00:11:58 -0500

    add

--- extension/src/components/panel/editor/EditorPanel.tsx
+++ extension/src/components/panel/editor/EditorPanel.tsx
@@ -131,7 +131,7 @@ const EditorPanel = () => {
                   type="button"
                   data-tooltip-target="tooltip-default"
                   onClick={pasteCode}
-                  className="text-white  justify-between hover:bg-fill-quaternary dark:hover:bg-fill-quaternary focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center me-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                  className="text-black dark:text-white justify-between hover:bg-fill-quaternary dark:hover:bg-fill-quaternary focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center me-2"
                 >
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
