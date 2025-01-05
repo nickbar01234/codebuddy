c6b7e32483609ab3793441913a3f1066e20244e4
HUNG NGO <hungngo1607@gmail.com>
2025-01-05 15:21:29 -0500

    refactor some icons and fix the sync code problem only in the background.ts (#62)

--- extension/src/components/navigator/Navigator.tsx
+++ extension/src/components/navigator/Navigator.tsx
@@ -5,6 +5,7 @@ import EditorPanel from "@cb/components/panel/editor";
 import { LoadingPanel } from "@cb/components/panel/LoadingPanel";
 import { stateContext } from "@cb/context/StateProvider";
 import { State } from "@cb/context/StateProvider";
+import { MenuIcon } from "@cb/components/icons";
 
 export const RootNavigator = () => {
   const { state } = React.useContext(stateContext);
@@ -25,6 +26,9 @@ export const RootNavigator = () => {
         className={`flex justify-between items-center w-full bg-[--color-tabset-tabbar-background] h-9 rounded-t-lg p-2 overflow-x-auto overflow-y-hidden`}
       >
         <h2 className="text-lg font-medium">Code Buddy</h2>
+        <button onClick={() => localStorage.clear()} className="text-sm">
+          Clear local storage
+        </button>
         <button
           className="hover:text-label-1 dark:hover:text-dark-label-1 flex cursor-pointer items-center justify-center rounded-md w-6 h-6 hover:bg-fill-secondary p-1"
           id="headlessui-menu-button-:r3q:"
@@ -34,20 +38,7 @@ export const RootNavigator = () => {
           data-headlessui-state=""
           onClick={() => setDisplayPopup((displayPopup) => !displayPopup)}
         >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            width="1em"
-            height="1em"
-            fill="currentColor"
-            className="hover:text-gray-7 dark:hover:text-dark-gray-7 text-text-secondary dark:text-text-secondary w-4 h-4"
-          >
-            <path
-              fill-rule="evenodd"
-              d="M4.4 14a2 2 0 100-4 2 2 0 000 4zm9.6-2a2 2 0 11-4 0 2 2 0 014 0zm7.6 0a2 2 0 11-4 0 2 2 0 014 0z"
-              clip-rule="evenodd"
-            />
-          </svg>
+          <MenuIcon />
         </button>
       </div>
       <div className="h-full w-full relative overflow-hidden ">
