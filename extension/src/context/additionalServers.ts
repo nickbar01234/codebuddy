387457760edf48a0c2acf06a398571e0d5485ed3
HUNG NGO <hungngo1607@gmail.com>
2025-01-06 00:24:07 -0500

    new turn

--- extension/src/context/additionalServers.ts
+++ extension/src/context/additionalServers.ts
@@ -1,38 +1,28 @@
 const remoteServers = [
   {
-    urls: "turn:relay1.expressturn.com:3478",
-    credential: "n249CgDuYqr0b4Sc",
-    username: "efBOWWM4SVBB1JE5CO",
-  },
-  {
-    urls: "turn:13.250.13.83:3478?transport=udp",
-    username: "YzYNCouZM1mhqhmseWk6",
-    credential: "YzYNCouZM1mhqhmseWk6",
-  },
-  {
     urls: "stun:stun.relay.metered.ca:80",
   },
   {
     urls: "turn:global.relay.metered.ca:80",
-    username: "fe645f487d27702ca414216a",
-    credential: "1RgQIEt3W2JcGbLL",
+    username: "eb9ec217ac64f46879e5e931",
+    credential: "uhHCX1lSii7KbQBE",
   },
   {
     urls: "turn:global.relay.metered.ca:80?transport=tcp",
-    username: "fe645f487d27702ca414216a",
-    credential: "1RgQIEt3W2JcGbLL",
+    username: "eb9ec217ac64f46879e5e931",
+    credential: "uhHCX1lSii7KbQBE",
   },
   {
     urls: "turn:global.relay.metered.ca:443",
-    username: "fe645f487d27702ca414216a",
-    credential: "1RgQIEt3W2JcGbLL",
+    username: "eb9ec217ac64f46879e5e931",
+    credential: "uhHCX1lSii7KbQBE",
   },
   {
     urls: "turns:global.relay.metered.ca:443?transport=tcp",
-    username: "fe645f487d27702ca414216a",
-    credential: "1RgQIEt3W2JcGbLL",
+    username: "eb9ec217ac64f46879e5e931",
+    credential: "uhHCX1lSii7KbQBE",
   },
 ];
 // TODO: Set to true to use the additional servers
-const useAdditionalServers = false;
+const useAdditionalServers = true;
 export const additionalServers = useAdditionalServers ? remoteServers : [];
