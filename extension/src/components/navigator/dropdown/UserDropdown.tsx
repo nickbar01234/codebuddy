706a9a3972009507e3b0a03573ffb568899af01b
HUNG NGO <hungngo1607@gmail.com>
2025-01-08 16:39:42 -0500

    Please dont delete this network indicator, my 2 hour :))

--- extension/src/components/navigator/dropdown/UserDropdown.tsx
+++ extension/src/components/navigator/dropdown/UserDropdown.tsx
@@ -7,38 +7,64 @@ interface UserDropdownProps {
   isOpen: boolean;
   toggle: (e: React.MouseEvent<Element, MouseEvent>) => void;
 }
+
+const GREENTHRESHOLD = 1250;
+const YELLOWTHRESHOLD = 1400;
+
 const UserDropdown: React.FC<UserDropdownProps> = ({ isOpen, toggle }) => {
   const { activePeer, peers, setActivePeerId } = usePeerSelection();
   const { peerState } = useRTC();
   const ping = Math.round(peerState[activePeer?.id || ""]?.latency);
-
+  const [isTooltipVisible, setTooltipVisible] = React.useState(false);
+  const signalStrength = getStatus(ping);
   return (
     activePeer && (
       <div>
-        <button
-          data-dropdown-toggle="dropdown"
-          className={cn(
-            "font-medium has-tooltip rounded-lg text-sm px-3 py-2 text-center inline-flex items-center hover:text-label-1 dark:hover:text-dark-label-1 hover:bg-fill-secondary relative"
-          )}
-          type="button"
-          onClick={toggle}
-        >
-          {activePeer.id} <CaretDownIcon />
-          <span
+        <div className="flex items-center">
+          <button
+            data-dropdown-toggle="dropdown"
             className={cn(
-              "tooltip rounded shadow-lg p-1 whitespace-nowrap text-xs absolute left-full ml-2 top-1/2 -translate-y-1/2",
-              ping === null
-                ? "text-red-500"
-                : ping < 1300
-                ? "text-green-500"
-                : ping < 1500
-                ? "text-yellow-500"
-                : "text-red-500"
+              "font-medium has-tooltip rounded-lg text-sm px-3 py-2 text-center inline-flex items-center hover:text-label-1 dark:hover:text-dark-label-1 hover:bg-fill-secondary relative"
             )}
+            type="button"
+            onClick={toggle}
+            onMouseEnter={() => setTooltipVisible(true)}
+            onMouseLeave={() => setTooltipVisible(false)}
           >
-            {ping !== null ? `${ping} ms` : "Error"}
-          </span>
-        </button>
+            {activePeer.id} <CaretDownIcon />
+            <span
+              className={cn(
+                "tooltip rounded shadow-lg p-1 whitespace-nowrap text-xs absolute font-bold left-full top-1/2 -translate-y-1/2",
+                signalStrength.text
+              )}
+            >
+              {ping !== null ? `${ping} ms` : "Error"}
+            </span>
+          </button>
+          {!isTooltipVisible && (
+            <div>
+              <i
+                className={cn(
+                  " rounded-[4px] inline-flex items-end justify-end w-auto h-[24px] p-[4px] icon__signal-strength z-50 "
+                )}
+              >
+                {Array.from({ length: 3 }).map((_, i) => (
+                  <span
+                    key={i}
+                    className={cn(
+                      `bar-${
+                        i + 1
+                      } inline-block w-[6px] ml-[2px] rounded-[2px]`,
+                      signalStrength.bg +
+                        " " +
+                        (i + 1 > signalStrength.level ? "opacity-20" : "")
+                    )}
+                  ></span>
+                ))}
+              </i>
+            </div>
+          )}
+        </div>
 
         <div
           className={`absolute z-50 bg-layer-3 dark:bg-dark-layer-3 border-divider-4 dark:border-dark-divider-4 shadow-level1 dark:shadow-dark-level1 divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 left-0 ${
@@ -62,5 +88,22 @@ const UserDropdown: React.FC<UserDropdownProps> = ({ isOpen, toggle }) => {
     )
   );
 };
+function getStatus(ping: number | null) {
+  const status =
+    ping === null
+      ? "red"
+      : ping < GREENTHRESHOLD
+      ? "green"
+      : ping < YELLOWTHRESHOLD
+      ? "yellow"
+      : "red";
 
+  const statusMapping = {
+    red: { bg: "bg-red-500", text: "text-red-500", level: 1 },
+    green: { bg: "bg-green-500", text: "text-green-500", level: 3 },
+    yellow: { bg: "bg-yellow-500", text: "text-yellow-500", level: 2 },
+  };
+
+  return { status, ...statusMapping[status] };
+}
 export default UserDropdown;
