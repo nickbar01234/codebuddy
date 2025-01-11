92edb79fb4695322edc8ee82db2e9cf6e136b965
nickbar01234 <nickbar01234@gmail.com>
2025-01-11 15:26:48 +0700

    Add local storage guard

--- extension/src/services/index.ts
+++ extension/src/services/index.ts
@@ -1,13 +1,36 @@
-import { ExtensionStorage, ServiceRequest, ServiceResponse } from "@cb/types";
+import {
+  ExtensionStorage,
+  LocalStorage,
+  ServiceRequest,
+  ServiceResponse,
+} from "@cb/types";
+
+const LOCAL_STORAGE_KEY = "codebuddy";
 
 export const sendServiceRequest = <T extends ServiceRequest>(
   request: T
 ): Promise<ServiceResponse[T["action"]]> => chrome.runtime.sendMessage(request);
 
-export const setStorage = (items: Partial<ExtensionStorage>) =>
+export const setChromeStorage = (items: Partial<ExtensionStorage>) =>
   chrome.storage.sync.set(items);
 
-export const getStorage = <K extends keyof ExtensionStorage>(key: K) =>
+export const getChromeStorage = <K extends keyof ExtensionStorage>(key: K) =>
   chrome.storage.sync.get(key).then((pref) => pref[key]) as Promise<
     ExtensionStorage[K]
   >;
+
+export const getLocalStorage = <K extends keyof LocalStorage>(key: K) => {
+  const store = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY) ?? "{}");
+  return store[key] as LocalStorage[K] | undefined;
+};
+
+export const setLocalStorage = (items: Partial<LocalStorage>) => {
+  const store = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY) ?? "{}");
+  localStorage.setItem(
+    LOCAL_STORAGE_KEY,
+    JSON.stringify({ ...store, ...items })
+  );
+};
+
+export const clearLocalStorage = () =>
+  localStorage.removeItem(LOCAL_STORAGE_KEY);
