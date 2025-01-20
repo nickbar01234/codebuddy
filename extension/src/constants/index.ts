272ee186acf86a9edec9ac771f72e336b967e0db
nickbar01234 <nickbar01234@gmail.com>
2025-01-20 18:17:42 +0700

    Start development with larger panel

--- extension/src/constants/index.ts
+++ extension/src/constants/index.ts
@@ -1,8 +1,11 @@
 import { ExtensionStorage } from "@cb/types";
 
+// todo(nickbar01234): Small hack since .ts file can't recognize client-side env
+const development = (import.meta as any).env.MODE;
+
 export const CodeBuddyPreference: ExtensionStorage = {
   appPreference: {
-    width: 300 /* px */,
+    width: development ? 600 : 300 /* px */,
     isCollapsed: false,
   },
   codePreference: {
