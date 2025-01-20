d38c790db90e3b0fb3d7b1c62469a496559be88f
nickbar01234 <nickbar01234@gmail.com>
2025-01-20 19:34:50 +0700

    Add firebase config to .gitignore

--- extension/src/db/config.ts
+++
@@ -1,9 +0,0 @@
-export const config = {
-  apiKey: "AIzaSyAKuruECIw23icIDj_CmrfjnRew7IzBeZo",
-  authDomain: "codebuddy-292ff.firebaseapp.com",
-  projectId: "codebuddy-292ff",
-  storageBucket: "codebuddy-292ff.firebasestorage.app",
-  messagingSenderId: "751823920040",
-  appId: "1:751823920040:web:62dc1b68fec2c5a3e45b9d",
-  measurementId: "G-VNLQK436QL",
-};
