9f47e633cb7d9779893d6cfe01b1ffde91f44046
nickbar01234 <nickbar01234@gmail.com>
2025-01-26 01:27:32 +0700

    Sign in with email link

--- extension/src/App.tsx
+++ extension/src/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import RootNavigator from "@cb/components/navigator/RootNavigator";
 import { AppPanel } from "@cb/components/panel";
 import SessionProvider from "./context/SessionProvider";
+<<<<<<< HEAD
 import { WindowProvider } from "./context/WindowProvider";
 
 const App = () => {
@@ -13,6 +14,16 @@ const App = () => {
         </SessionProvider>
       </AppPanel>
     </WindowProvider>
+=======
+
+const App = () => {
+  return (
+    <AppPanel>
+      <SessionProvider>
+        <RootNavigator />
+      </SessionProvider>
+    </AppPanel>
+>>>>>>> 7a16449 (Sign in with email link)
   );
 };
 
