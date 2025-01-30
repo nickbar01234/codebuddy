c522521fbcb536de81b2ed03ae95d7aeda32f1ff
Nick Doan <nickbar01234@gmail.com>
2025-01-30 10:29:08 +0700

    Reclaim resources (#109)
    * Reclaim resources
    
    * Tidy up implementation

---
+++ extension/src/hooks/useResource.ts
@@ -0,0 +1,65 @@
+import React from "react";
+import { useOnMount } from ".";
+
+type Unsubscribe<T> = (resource: T) => void;
+
+interface Resource<T> {
+  value: T;
+  unsubscribe: Unsubscribe<T>;
+}
+
+interface UseResourceProps {
+  name?: string;
+}
+
+const useResource = <T>({ name }: UseResourceProps) => {
+  const resourceRef = React.useRef<Record<string, Resource<T> | undefined>>({});
+
+  const register = React.useRef(
+    (key: string, value: T, unsubscribe: (resource: T) => void) => {
+      evict(key);
+      resourceRef.current[key] = { value, unsubscribe };
+    }
+  ).current;
+
+  const get = React.useRef((key: string) => resourceRef.current[key]).current;
+
+  const set = React.useRef((key: string, cb: (resource: T) => T) => {
+    if (resourceRef.current[key] != undefined) {
+      const { unsubscribe, value } = resourceRef.current[key] as Resource<T>;
+      resourceRef.current[key] = {
+        unsubscribe,
+        value: cb(value),
+      };
+    }
+  }).current;
+
+  const evict = React.useRef((key: string) => {
+    const resource = resourceRef.current[key];
+    if (resource != undefined) {
+      const { value, unsubscribe } = resource;
+      unsubscribe(value);
+      delete resourceRef.current[key];
+    }
+  }).current;
+
+  const cleanup = React.useRef(() => {
+    const resources = Object.keys(resourceRef.current);
+    console.log(`Cleaning up ${name ?? "resource"}`, resources);
+    resources.forEach(evict);
+  }).current;
+
+  useOnMount(() => {
+    return () => cleanup();
+  });
+
+  return {
+    register,
+    get,
+    cleanup,
+    set,
+    evict,
+  };
+};
+
+export default useResource;
