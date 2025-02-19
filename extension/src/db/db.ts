e26166f9a06a0a109317e78c334d43aad6c4f5c4
HUNG NGO <hungngo1607@gmail.com>
2025-02-18 23:42:07 -0500

    add

--- extension/src/db/db.ts
+++ extension/src/db/db.ts
@@ -1,9 +1,8 @@
 import { firebaseOptions } from "@cb/constants";
-import { getLocalStorage } from "@cb/services";
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { connectAuthEmulator } from "firebase/auth/web-extension";
-import { getFirestore } from "firebase/firestore";
+import { connectFirestoreEmulator, getFirestore } from "firebase/firestore";
 
 const app = initializeApp(firebaseOptions);
 
@@ -13,10 +12,9 @@ const firestore = getFirestore(app);
 
 if (import.meta.env.MODE === "development") {
   // See firebase.json
-  const BASE_URL = getLocalStorage("firebaseURL") ?? "http://127.0.0.1:9099";
-  console.log("Using Firebase Emulator at", BASE_URL);
-  const AUTH_URL = `${BASE_URL}`;
+  const AUTH_URL = "http://localhost:3003";
   connectAuthEmulator(auth, AUTH_URL);
+  connectFirestoreEmulator(firestore, "localhost", 3001);
 }
 
 export { auth, firestore };
