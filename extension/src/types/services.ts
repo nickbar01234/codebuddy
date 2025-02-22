97e0e13bbcb670138c9b57a0f9897203d5cd8f00
nickbar01234 <nickbar01234@gmail.com>
2025-02-22 01:45:20 -0500

    Close previous tab on initial signin

--- extension/src/types/services.ts
+++ extension/src/types/services.ts
@@ -1,3 +1,4 @@
+import { LocalStorage } from ".";
 import type {
   GenericMessage,
   GenericResponse,
@@ -35,13 +36,24 @@ interface ReloadExtensionRequest extends GenericMessage {
   action: "reloadExtension";
 }
 
+interface GetActiveTabIdRequest extends GenericMessage {
+  action: "getActiveTabId";
+}
+
+interface CloseSignInTabRequest extends GenericMessage {
+  action: "closeSignInTab";
+  signIn: LocalStorage["signIn"];
+}
+
 export type ServiceRequest =
   | GetValueRequest
   | PastCodeRequest
   | SetupCodeBuddyModel
   | SetOtherEditorRequest
   | SetupLeetCodeModel
-  | ReloadExtensionRequest;
+  | ReloadExtensionRequest
+  | GetActiveTabIdRequest
+  | CloseSignInTabRequest;
 
 export enum ResponseStatus {
   SUCCESS,
@@ -64,5 +76,7 @@ export type ServiceResponse = GenericResponse<
     setupLeetCodeModel: ServiceGenericResponse;
     setValueOtherEditor: void;
     reloadExtension: void;
+    getActiveTabId: number;
+    closeSignInTab: ServiceGenericResponse;
   }
 >;
