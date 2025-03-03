2a1791b641bed2b2d95d6979f6d838c4d64e9958
Van Nguyen <87792873+yourvivian@users.noreply.github.com>
2025-03-02 19:14:55 -0800

    connect firestore emulator

--- extension/src/constants/index.ts
+++ extension/src/constants/index.ts
@@ -17,7 +17,7 @@ export const CodeBuddyPreference: ExtensionStorage = {
 export const firebaseOptions: FirebaseOptions = {
   apiKey: env.VITE_API_KEY,
   authDomain: env.VITE_AUTH_DOMAIN,
-  projectId: env.VITE_PROJECT_ID,
+  projectId: env.MODE === "development" ? "demo-code-buddy-development" : env.VITE_PROJECT_ID,
   storageBucket: env.VITE_STORAGE_BUCKET,
   messagingSenderId: env.VITE_MESSAGING_SENDER_ID,
   appId: env.VITE_APP_ID,
