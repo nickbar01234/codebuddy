c4dd0fb4cc5d88823e4a75a2b160c33f2710ea19
Long Dau <135937853+dtnlong1@users.noreply.github.com>
2025-03-02 19:30:25 -0600

    current implementation for a button (#146)
    * current implementation for a button
    
    * second version-first button
    
    * second version-first button
    
    * simplify hook
    
    * delete the hook
    
    * implemented buttons' thrrotle functions
    
    * implemented buttons' thrrotle functions
    
    * change implementation
    
    * Fix indentation
    
    ---------
    
    Co-authored-by: nickbar01234 <nickbar01234@gmail.com>

--- extension/src/components/navigator/menu/RoomControlMenu.tsx
+++ extension/src/components/navigator/menu/RoomControlMenu.tsx
@@ -27,23 +27,23 @@ import {
 } from "@cb/lib/components/ui/dialog";
 import { DialogDescription } from "@radix-ui/react-dialog";
 import { RoomControlDropdownMenuItem } from "./RoomControlDropdownMenuItem";
+import { throttle } from "lodash";
 
-const _RoomControlMenu = () => {
-  const { createRoom, joinRoom, roomId, leaveRoom } = useRTC();
-  const { state: appState, setState: setAppState } =
-    React.useContext(appStateContext);
-  const [inputRoomId, setInputRoomId] = React.useState("");
-
-  const onJoinRoom = async (
-    e: React.MouseEvent<Element, MouseEvent> | React.KeyboardEvent<Element>
-  ) => {
-    e.stopPropagation();
-    const haveJoined = await joinRoom(inputRoomId);
-    if (haveJoined) {
-      setAppState(AppState.ROOM);
-    }
-  };
-
+const _RoomControlMenu = ({
+  appState,
+  onCreateRoom,
+  onJoinRoom,
+  onLeaveRoom,
+  inputRoomId,
+  setInputRoomId,
+}: {
+  appState: AppState;
+  onCreateRoom: (e: Event) => void;
+  onJoinRoom: (e: React.MouseEvent | React.KeyboardEvent) => void;
+  onLeaveRoom: (e: Event) => void;
+  inputRoomId: string;
+  setInputRoomId: React.Dispatch<React.SetStateAction<string>>;
+}) => {
   const onChangeRoomIdInput = (e: React.ChangeEvent<HTMLInputElement>) => {
     e.preventDefault();
     setInputRoomId(e.target.value);
@@ -53,13 +53,7 @@ const _RoomControlMenu = () => {
     case AppState.HOME:
       return (
         <>
-          <RoomControlDropdownMenuItem
-            onSelect={(e) => {
-              e.stopPropagation();
-              setAppState(AppState.ROOM);
-              createRoom({});
-            }}
-          >
+          <RoomControlDropdownMenuItem onSelect={onCreateRoom}>
             <span className="flex gap-2 items-center">
               <PlusIcon /> Create Room
             </span>
@@ -84,8 +78,7 @@ const _RoomControlMenu = () => {
                   onChange={onChangeRoomIdInput}
                   onKeyDown={(e) => {
                     if (e.key === "Enter") {
-                      e.stopPropagation();
-                      onJoinRoom(e as React.KeyboardEvent<Element>);
+                      onJoinRoom(e);
                     } // Trigger the join room action
                   }}
                 />
@@ -101,20 +94,14 @@ const _RoomControlMenu = () => {
           <RoomControlDropdownMenuItem
             onSelect={(e) => {
               e.stopPropagation();
-              navigator.clipboard.writeText(roomId ?? "");
+              navigator.clipboard.writeText(inputRoomId ?? "");
             }}
           >
             <span className="flex gap-2 items-center">
               <CopyIcon /> Copy Room ID
             </span>
           </RoomControlDropdownMenuItem>
-          <RoomControlDropdownMenuItem
-            onSelect={(e) => {
-              e.stopPropagation();
-              setAppState(AppState.HOME);
-              if (roomId) leaveRoom(roomId);
-            }}
-          >
+          <RoomControlDropdownMenuItem onSelect={onLeaveRoom}>
             <span className="flex gap-2 items-center">
               <LeaveIcon /> Leave Room
             </span>
@@ -128,8 +115,10 @@ const _RoomControlMenu = () => {
 };
 
 export const RoomControlMenu = () => {
-  const { roomId, leaveRoom } = useRTC();
-  const { setState: setAppState } = React.useContext(appStateContext);
+  const { createRoom, joinRoom, roomId, leaveRoom } = useRTC();
+  const { state: appState, setState: setAppState } =
+    React.useContext(appStateContext);
+  const [inputRoomId, setInputRoomId] = React.useState("");
 
   React.useEffect(() => {
     if (roomId != null) {
@@ -139,26 +128,72 @@ export const RoomControlMenu = () => {
     }
   }, [roomId, setAppState]);
 
+  const createRoomThrottled = React.useMemo(() => {
+    return throttle((event: Event) => {
+      event.stopPropagation?.();
+      setAppState(AppState.ROOM);
+      createRoom({});
+    }, 1000);
+  }, [createRoom, setAppState]);
+
+  const joinRoomThrottled = React.useMemo(() => {
+    return throttle(
+      async (
+        reactEvent: React.MouseEvent<Element> | React.KeyboardEvent<Element>
+      ) => {
+        reactEvent.stopPropagation();
+        const haveJoined = await joinRoom(inputRoomId);
+        if (haveJoined) {
+          setAppState(AppState.ROOM);
+        }
+      },
+      1000
+    );
+  }, [joinRoom, inputRoomId, setAppState]);
+
+  const signOutThrottled = React.useMemo(() => {
+    return throttle(() => {
+      leaveRoom(roomId).then(() => signOut(auth));
+    }, 1000);
+  }, [leaveRoom, roomId]);
+
+  const resetExtensionThrottled = React.useMemo(() => {
+    return throttle((event: Event) => {
+      event.stopPropagation?.();
+      clearLocalStorage();
+    }, 1000);
+  }, []);
+
+  const leaveRoomThrottled = React.useMemo(() => {
+    return throttle((event: Event) => {
+      event.stopPropagation?.();
+      setAppState(AppState.HOME);
+      if (roomId) {
+        leaveRoom(roomId);
+      }
+    }, 1000);
+  }, [roomId, leaveRoom, setAppState]);
+
   return (
     <DropdownMenu>
       <DropdownMenuTrigger>
         <MenuIcon />
       </DropdownMenuTrigger>
       <DropdownMenuContent className="absolute right-0 top-2 shadow-level3 dark:shadow-dark-level3 rounded-lg border border-border-tertiary dark:border-border-tertiary bg-layer-02 dark:bg-layer-02 w-max flex flex-col">
-        <_RoomControlMenu />
-        <RoomControlDropdownMenuItem
-          onSelect={() => leaveRoom(roomId).then(() => signOut(auth))}
-        >
+        <_RoomControlMenu
+          appState={appState}
+          onCreateRoom={createRoomThrottled}
+          onJoinRoom={joinRoomThrottled}
+          onLeaveRoom={leaveRoomThrottled}
+          inputRoomId={inputRoomId}
+          setInputRoomId={setInputRoomId}
+        />
+        <RoomControlDropdownMenuItem onSelect={signOutThrottled}>
           <span className="flex gap-2 items-center">
             <SignOutIcon /> <span>Sign Out</span>
           </span>
         </RoomControlDropdownMenuItem>
-        <RoomControlDropdownMenuItem
-          onSelect={(e) => {
-            clearLocalStorage();
-            e.stopPropagation();
-          }}
-        >
+        <RoomControlDropdownMenuItem onSelect={resetExtensionThrottled}>
           <span className="flex gap-2 items-center">
             <ResetIcon /> <span>Reset Extension</span>
           </span>
