2b014fcf43ab5536fc1646715011c1c56c8d2e5a
HUNG NGO <hungngo1607@gmail.com>
2025-03-02 23:53:56 -0500

    fix done

--- extension/src/components/navigator/RootNavigator.tsx
+++ extension/src/components/navigator/RootNavigator.tsx
@@ -1,16 +1,15 @@
-import { Status } from "@cb/types";
-import SignInPanel from "@cb/components/panel/SignInPanel";
-import useDevReload from "@cb/hooks/useDevReload";
-import { useSession } from "@cb/hooks/index";
-import { RTCProvider } from "@cb/context/RTCProvider";
-import { PeerSelectionProvider } from "@cb/context/PeerSelectionProvider";
-import { AppNavigator } from "./AppNavigator";
 import { LoadingPanel } from "@cb/components/panel/LoadingPanel";
+import SignInPanel from "@cb/components/panel/SignInPanel";
 import { AppStateProvider } from "@cb/context/AppStateProvider";
+import { PeerSelectionProvider } from "@cb/context/PeerSelectionProvider";
+import { RTCProvider } from "@cb/context/RTCProvider";
+import { useSession } from "@cb/hooks/index";
+import { Status } from "@cb/types";
+import { AppNavigator } from "./AppNavigator";
 
 const RootNavigator = () => {
   const { auth } = useSession();
-  useDevReload();
+  // useDevReload();
 
   switch (auth.status) {
     case Status.AUTHENTICATED:
