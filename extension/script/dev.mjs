dbb2a32601d8c223ea51b951be51e844d8e4f61c
nickbar01234 <nickbar01234@gmail.com>
2025-03-04 23:33:20 -0500

    Use email

--- extension/script/dev.mjs
+++ extension/script/dev.mjs
@@ -10,7 +10,12 @@ const TARGET_QUESTION = "https://leetcode.com/problems/two-sum/";
 // eslint-disable-next-line no-undef
 const NUM_USERS = process.env.USERS ?? 2;
 
-const USERNAMES = ["code", "buddy", "dev", "mode"];
+const USERNAMES = [
+  "code@gmail.com",
+  "buddy@hotmail.com",
+  "dev@outlook.com",
+  "mode@yahoo.com",
+];
 const PAGES = Array.from({ length: NUM_USERS });
 const PEERS = Array.from({ length: NUM_USERS }).map((_, idx) => ({
   peer: USERNAMES[idx],
