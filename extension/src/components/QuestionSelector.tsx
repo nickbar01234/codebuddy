276f3fb4c02cbe8cd8f64d4198a67532b5b08fe4
HUNG NGO <hungngo1607@gmail.com>
2025-03-09 01:20:59 -0500

    adding overloading

--- extension/src/components/QuestionSelector.tsx
+++ extension/src/components/QuestionSelector.tsx
@@ -1,43 +1,62 @@
-import React from 'react';
-import { useOnMount } from '@cb/hooks';
-import { waitForElement } from '@cb/utils';
+import React from "react";
+import { useOnMount } from "@cb/hooks";
+import { waitForElement } from "@cb/utils";
 
 export default function QuestionSelector() {
     const [question, setQuestion] = React.useState<string>("");
 
     useOnMount(() => {
-    console.log("mounted");
-    waitForElement("#leetcode_question", 2000).then((element) => {
-    const iframe =  element as HTMLIFrameElement;
-    console.log(iframe);
-    iframe.onload = () => {
-        iframe.contentDocument?.addEventListener("click", (e) => {
-            console.log("clicked");
-        }
-        );
-        const questions = iframe.contentDocument?.querySelectorAll('[role="row"]') as unknown as NodeListOf<HTMLDivElement>;
-        if (questions != null) {
-            for (let i = 0; i < questions.length; i++) {
-                questions[i].addEventListener("click", (e) => {
-                    console.log("clicked");
-                    const question = e.target as HTMLDivElement;
-                    const questionTitle = question.querySelector('a') as HTMLAnchorElement;
-                    console.log(questionTitle.href);
+        console.log("mounted");
+        waitForElement("#leetcode_question", 2000).then((element) => {
+            const iframe = element as HTMLIFrameElement;
+            iframe.onload = async () => {
+                const iframeDoc =
+                    iframe.contentDocument ||
+                    (iframe.contentWindow && iframe.contentWindow.document);
+                if (iframeDoc) {
+                    waitForElement(
+                        "div[role='rowgroup']",
+                        3000,
+                        iframeDoc as Document
+                    ).then((element) => {
+                        const questions =
+                            iframe.contentWindow?.document.querySelectorAll(
+                                "div[role='row']"
+                            ) as NodeListOf<HTMLDivElement>;
+                        if (questions != null) {
+                            for (let i = 0; i < questions.length; i++) {
+                                waitForElement(
+                                    "a",
+                                    3000,
+                                    questions[i] as unknown as Document
+                                ).then((element) => {
+                                    const link =
+                                        questions[i].querySelector("a");
+                                    if (link) {
+                                        const questionTitle =
+                                            link as HTMLAnchorElement;
+                                        questions[i].addEventListener(
+                                            "click",
+                                            (e) => {
+                                                console.log(questionTitle.href);
+                                            }
+                                        );
+                                    }
+                                });
+                            }
+                        }
+                    });
                 }
-                );
-            }
-        }
-    }
-    });
+            };
+        });
     });
     return (
-        <iframe 
-        src="https://leetcode.com/problemset/"
-        title="LeetCode Question"
-        id = "leetcode_question"
-        className="w-full h-full border-none overflow-auto z-100"
-           sandbox="allow-scripts allow-same-origin"
-        >
-        </iframe>
-    )
+        <iframe
+            src="https://leetcode.com/problemset/"
+            title="LeetCode Question"
+            id="leetcode_question"
+            className="z-100 h-full w-full overflow-auto border-none"
+            sandbox="allow-scripts allow-same-origin"
+        ></iframe>
+    );
 }
