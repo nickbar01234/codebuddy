04bb5c4efd2fa34f0f40ee8deeda4b6f593ba7d2
nickbar01234 <nickbar01234gmail.com>
2025-03-14 19:51:11 -0400

    Migrate eslint to flag config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,6 +2,10 @@
 
 import eslint from "@eslint/js";
 import tseslint from "typescript-eslint";
+import react from "eslint-plugin-react";
+import reactHooks from "eslint-plugin-react-hooks";
+import tsEslintPlugin from "@typescript-eslint/eslint-plugin";
+import globals from "globals";
 
 export default tseslint.config(
   eslint.configs.recommended,
@@ -12,6 +16,24 @@ export default tseslint.config(
       "no-explicit-any": "off",
       "@typescript-eslint/no-unused-vars": "warn",
       "@typescript-eslint/no-explicit-any": "warn",
+    },
+  },
+  {
+    files: ["extension/src/**/*.{js,ts,tsx}"],
+    ...react.configs.flat.recommended,
+    plugins: {
+      "react-hooks": reactHooks,
+      "ts-eslint": tsEslintPlugin,
+    },
+    rules: {
+      "react-hooks/rules-of-hooks": "error",
+      "react-hooks/exhaustive-deps": "error",
+    },
+    languageOptions: {
+      ...react.configs.flat.recommended.languageOptions,
+      globals: {
+        ...globals.browser,
+      },
     },
   }
 );
