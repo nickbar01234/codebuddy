a59f6f458075abe3e143d7887343936282b67eb6
Van Nguyen <87792873+yourvivian@users.noreply.github.com>
2025-03-17 16:26:54 -0700

    update usePaginate hook

--- extension/src/hooks/usePaginate.ts
+++ extension/src/hooks/usePaginate.ts
@@ -1,25 +1,24 @@
 import { useState, useEffect, useMemo } from "react";
 import {
   query as firestoreQuery,
-  limit,
   Query,
   getDocs,
   startAfter,
-  onSnapshot,
-  DocumentData,
   QuerySnapshot,
   getCountFromServer,
   QueryDocumentSnapshot,
+  limit as firestoreLimit,
+  QueryConstraint,
 } from "firebase/firestore";
 
 type Data<T> = {
-  totalDocs: number;
-  totalPages: number;
   currentPage: number;
   docs: QueryDocumentSnapshot<T>[];
 };
 
 type HookReturnValue<T> = {
+  count: number;
+  totalPages: number;
   data: Data<T>;
   error?: Error;
   loading: boolean;
@@ -35,75 +34,57 @@ type HookProps<T> = {
   from?: QueryDocumentSnapshot<T>;
 };
 
-const addQuery = <T>(q: Query<T>, fun: (val: any) => any, value: any) =>
+const addQuery = <T>(q: Query<T>, fun: (val: any) => QueryConstraint, value: any) =>
   value ? firestoreQuery(q, fun(value)) : q;
 
 const usePaginate = <T>({
-  query: mainQuery,
-  limit: pageSize,
-  from,
+  query: baseQuery,
+  limit,
+  // from,
 }: HookProps<T>): HookReturnValue<T> => {
   const [error, setError] = useState<Error>();
   const [loading, setLoading] = useState<boolean>(false);
   const [docs, setDocs] = useState<QueryDocumentSnapshot<T>[]>([]);
   const [lastSnap, setLastSnap] = useState<QueryDocumentSnapshot<T>[]>([]);
-  const [query, setQuery] = useState(addQuery(mainQuery, limit, pageSize));
-  const [totals, setTotals] = useState<
-    Pick<Data<T>, "totalDocs" | "totalPages">
-  >({
-    totalDocs: 0,
-    totalPages: 0,
-  });
+  const [query, setQuery] = useState(
+    addQuery(baseQuery, firestoreLimit, limit)
+  );
+  const [count, setCount] = useState(0);
+  const [totalPages, setTotalPages] = useState(0);
 
   const onRes = (res: QuerySnapshot<T>) => {
-    // console.log('onRes called with:', res.docs);
-    if (res.docs.length) {
-      // setLastSnap((e) => [...e, res.docs[pageSize - 1]]);
-      setLastSnap([res.docs[pageSize - 1]]);
-      // setDocs((e) => [...e, ...res.docs]);
+    if (res.size) {
+      setLastSnap((e) => [...e, res.docs[res.size - 1]]);
       setDocs(res.docs);
     }
     setLoading(false);
   };
 
   const onErr = (err: Error) => {
-    // console.error('Error fetching documents:', err);
     setError(err);
     setLoading(false);
   };
 
   useEffect(() => {
-    // console.log('Fetching documents with query:', query);
     setLoading(true);
-    getDocs(query).then(onRes).catch(onErr);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [query]);
+    getCountFromServer(baseQuery).then((res) => {
+      setTotalPages(Math.ceil(res.data().count / limit));
+      setCount(res.data().count);
+    });
+  }, [baseQuery, limit]);
 
   useEffect(() => {
-    // console.log('Fetching total document count for query:', mainQuery);
     setLoading(true);
-    getCountFromServer(mainQuery).then((res) => {
-      setQuery(addQuery(mainQuery, limit, pageSize));
-      setTotals({
-        totalDocs: res.data().count,
-        totalPages: Math.ceil(res.data().count / pageSize),
-      });
-    });
-  }, [mainQuery, pageSize]);
+    getDocs(query).then(onRes).catch(onErr);
+  }, [query]);
 
   const getLastEle = (array: any[]) => array[array.length - 1];
 
   const getNext = () => {
-    // if (lastSnap.length < totals.totalPages) {
-    //   let q = addQuery(mainQuery, startAfter, getLastEle(lastSnap));
-    //   q = addQuery(q, limit, pageSize);
-    //   setQuery(q);
-    // }
     console.log("getNext clicked. Last Snapshot:", lastSnap);
-    if (lastSnap.length && lastSnap[0]) {
-      console.log("Fetching next page after:", lastSnap[0].id);
-      let q = addQuery(mainQuery, startAfter, lastSnap[0]); // Use only last item
-      q = addQuery(q, limit, pageSize);
+    if (lastSnap.length) {
+      let q = addQuery(baseQuery, startAfter, lastSnap[lastSnap.length - 1]); // Use only last item
+      q = addQuery(q, firestoreLimit, limit);
       setQuery(q);
     } else {
       console.warn("No last snapshot available. Cannot fetch next page.");
@@ -114,29 +95,31 @@ const usePaginate = <T>({
     if (lastSnap.length > 1) {
       const newArray = lastSnap.slice(0, -2);
       setLastSnap(newArray);
-      let q = addQuery(mainQuery, startAfter, getLastEle(newArray));
-      q = addQuery(q, limit, pageSize);
+      let q = addQuery(baseQuery, startAfter, getLastEle(newArray));
+      q = addQuery(q, firestoreLimit, limit);
       setQuery(q);
     }
   };
 
-  return useMemo(
+  const memoizedResult = useMemo(
     () => ({
       error,
       loading,
       getNext,
       getPrevious,
-      hasNext: lastSnap.length < totals.totalPages,
+      hasNext: lastSnap.length < totalPages,
       hasPrevious: lastSnap.length > 1,
       data: {
         docs,
-        ...totals,
         currentPage: lastSnap.length,
       },
+      totalPages,
+      count,
     }),
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    [docs]
+    [docs, lastSnap, totalPages, error, count] // query causes re-rendering
   );
+
+  return memoizedResult;
 };
 
 export default usePaginate;
