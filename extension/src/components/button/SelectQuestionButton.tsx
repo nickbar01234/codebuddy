b363509359d574c1e82669221e7d0bc6ea0a7d11
nickbar01234 <nickbar01234gmail.com>
2025-03-23 14:49:06 -0400

    Align columns

--- extension/src/components/button/SelectQuestionButton.tsx
+++ extension/src/components/button/SelectQuestionButton.tsx
@@ -4,7 +4,8 @@ interface SelectButtonQuestionProps {
 
 const SelectQuestionButton = ({ href }: SelectButtonQuestionProps) => {
   return (
-    <div className="mx-2 flex items-center py-[11px]" role="cell">
+    // todo(nickbar01234) - Adding manual margin left to align the other columns. We should find a better method
+    <div className="ml-7 mr-2 flex items-center py-[11px]" role="cell">
       <button
         className="rounded-md p-2 text-white"
         style={{ backgroundColor: "#DD5471" }}
