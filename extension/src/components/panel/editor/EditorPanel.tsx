8cd4c94c15e530a27b4360128c0e15e3c2c1ae50
HUNG NGO <hungngo1607@gmail.com>
2025-03-23 22:34:06 -0400

    Hung/move_test_case (#173)
    * adding shadcn tabs
    
    * adding tabs
    
    * done styling
    
    * done
    
    * done styling
    
    * add
    
    * add
    
    * add
    
    * adjust padding
    
    * add
    
    * fix change
    
    * add
    
    * test
    
    * Revert "add"
    
    This reverts commit 283b6c774ac38fe8c0ae037b7b301deec417cea8.
    
    * add
    
    * Revert "test"
    
    This reverts commit 630715934c06134a36d297de0ff8a242f89e2097.
    
    * test
    
    * Force tab content to mount (#178)
    
    Co-authored-by: nickbar01234 <nickbar01234gmail.com>
    
    * add
    
    * done
    
    * add fixed length
    
    ---------
    
    Co-authored-by: Nick Doan <nickbar01234@gmail.com>

--- extension/src/components/panel/editor/EditorPanel.tsx
+++ extension/src/components/panel/editor/EditorPanel.tsx
@@ -1,3 +1,4 @@
+import UserDropdown from "@cb/components/navigator/dropdown/UserDropdown";
 import { LoadingPanel } from "@cb/components/panel/LoadingPanel";
 import { AppState } from "@cb/context/AppStateProvider";
 import {
@@ -5,10 +6,18 @@ import {
   usePeerSelection,
   useWindowDimensions,
 } from "@cb/hooks/index";
+import {
+  Tabs,
+  TabsContent,
+  TabsList,
+  TabsTrigger,
+} from "@cb/lib/components/ui/tabs";
 import { cn } from "@cb/utils/cn";
+import { CodeXml, FlaskConical } from "lucide-react";
 import React from "react";
 import { ResizableBox } from "react-resizable";
 import EditorToolBar from "./EditorToolBar";
+import { Separator } from "@cb/lib/components/ui/separator";
 
 export interface TabMetadata {
   id: string;
@@ -27,6 +36,14 @@ const EditorPanel = () => {
     preference: { codePreference },
     height,
   } = useWindowDimensions();
+  const [isUserDropdownOpen, setUserDropdownOpen] = React.useState(false);
+  const toggleUserDropdown = React.useCallback(
+    (e: React.MouseEvent<Element, MouseEvent>) => {
+      e.stopPropagation();
+      setUserDropdownOpen((prev) => !prev);
+    },
+    []
+  );
 
   const canViewCode = activePeer?.viewable ?? false;
   const activeTest = activePeer?.tests.find((test) => test.selected);
@@ -73,58 +90,103 @@ const EditorPanel = () => {
             onResize={(_e, data) => setCodePreferenceHeight(data.size.height)}
             onResizeStop={onResizeStop}
           >
-            <div className="relative flex h-full w-full grow flex-col gap-y-2">
-              <EditorToolBar />
-              <div
-                id={EDITOR_NODE_ID}
-                className="h-full w-full overflow-hidden"
-              />
-            </div>
+            <Tabs defaultValue="code" className="h-full w-full">
+              <TabsList className="flex w-full justify-start gap-2">
+                <UserDropdown
+                  key={"user-dropdown"}
+                  isOpen={isUserDropdownOpen}
+                  toggle={toggleUserDropdown}
+                />
+
+                <Separator
+                  orientation="vertical"
+                  className="flexlayout__tabset_tab_divider h-[1rem] bg-[--color-tabset-tabbar-background]"
+                />
+                <TabsTrigger
+                  value="code"
+                  className="rounded-none border-transparent bg-transparent hover:rounded-t-sm hover:bg-[--color-tabset-tabbar-background] data-[state=active]:border-b-2 data-[state=active]:border-orange-500 data-[state=active]:bg-transparent"
+                >
+                  <CodeXml className="mr-2 h-4 w-4 text-green-500" />
+                  Code
+                </TabsTrigger>
+                <Separator
+                  orientation="vertical"
+                  className="flexlayout__tabset_tab_divider h-[1rem] bg-[--color-tabset-tabbar-background]"
+                />
+                <TabsTrigger
+                  value="test"
+                  className="rounded-none border-transparent bg-transparent hover:rounded-t-sm hover:bg-[--color-tabset-tabbar-background] data-[state=active]:border-b-2 data-[state=active]:border-orange-500 data-[state=active]:bg-transparent"
+                >
+                  <FlaskConical className="mr-2 h-4 w-4 text-green-500" />
+                  Test
+                </TabsTrigger>
+              </TabsList>
+              <TabsContent
+                value="code"
+                forceMount
+                className={cn("data-[state=inactive]:hidden")}
+              >
+                <div className="h-full w-full">
+                  <EditorToolBar />
+                  <div
+                    id={EDITOR_NODE_ID}
+                    className="h-full min-h-[50vh] w-full overflow-hidden"
+                  />
+                </div>
+              </TabsContent>
+              <TabsContent
+                value="test"
+                forceMount
+                className={cn("data-[state=inactive]:hidden")}
+              >
+                <div className="mx-5 my-4 flex h-full w-full flex-col space-y-4">
+                  <div className="flex w-full flex-row items-start justify-between gap-4">
+                    <div className="hide-scrollbar flex flex-nowrap items-center gap-x-2 gap-y-4 overflow-x-scroll">
+                      {activePeer?.tests.map((test, idx) => (
+                        <div key={idx} onClick={() => selectTest(idx)}>
+                          {test.selected ? (
+                            <button className="bg-fill-3 dark:bg-dark-fill-3 hover:bg-fill-2 dark:hover:bg-dark-fill-2 hover:text-label-1 dark:hover:text-dark-label-1 text-label-1 dark:text-dark-label-1 relative inline-flex items-center whitespace-nowrap rounded-lg px-4 py-1 font-medium focus:outline-none">
+                              Case {idx + 1}
+                            </button>
+                          ) : (
+                            <button className="hover:bg-fill-2 dark:hover:bg-dark-fill-2 text-label-2 dark:text-dark-label-2 hover:text-label-1 dark:hover:text-dark-label-1 dark:bg-dark-transparent relative inline-flex items-center whitespace-nowrap rounded-lg bg-transparent px-4 py-1 font-medium focus:outline-none">
+                              Case {idx + 1}
+                            </button>
+                          )}
+                        </div>
+                      ))}
+                    </div>
+                  </div>
+                  <div className="space-y-4">
+                    <div>
+                      <div className="flex h-full w-full flex-col space-y-2">
+                        {activeTest?.test.map((assignment, idx) => (
+                          <React.Fragment key={idx}>
+                            <div className="text-label-3 dark:text-dark-label-3 text-xs font-medium">
+                              {assignment.variable} =
+                            </div>
+                            <div className="font-menlo bg-fill-3 dark:bg-dark-fill-3 w-full cursor-text rounded-lg border border-transparent px-3 py-[10px]">
+                              <div
+                                className="font-menlo placeholder:text-label-4 dark:placeholder:text-dark-label-4 sentry-unmask w-full resize-none whitespace-pre-wrap break-words outline-none"
+                                contentEditable="true"
+                              >
+                                {assignment.value}
+                              </div>
+                            </div>
+                          </React.Fragment>
+                        )) ?? null}
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </TabsContent>
+            </Tabs>
           </ResizableBox>
           <div
             className="relative w-full overflow-auto"
             style={{ height: height - codePreference.height - 128 }}
           >
-            <div className="mx-5 my-4 flex flex-col space-y-4">
-              <div className="flex w-full flex-row items-start justify-between gap-4">
-                <div className="hide-scrollbar flex flex-nowrap items-center gap-x-2 gap-y-4 overflow-x-scroll">
-                  {activePeer?.tests.map((test, idx) => (
-                    <div key={idx} onClick={() => selectTest(idx)}>
-                      {test.selected ? (
-                        <button className="bg-fill-3 dark:bg-dark-fill-3 hover:bg-fill-2 dark:hover:bg-dark-fill-2 hover:text-label-1 dark:hover:text-dark-label-1 text-label-1 dark:text-dark-label-1 relative inline-flex items-center whitespace-nowrap rounded-lg px-4 py-1 font-medium focus:outline-none">
-                          Case {idx + 1}
-                        </button>
-                      ) : (
-                        <button className="hover:bg-fill-2 dark:hover:bg-dark-fill-2 text-label-2 dark:text-dark-label-2 hover:text-label-1 dark:hover:text-dark-label-1 dark:bg-dark-transparent relative inline-flex items-center whitespace-nowrap rounded-lg bg-transparent px-4 py-1 font-medium focus:outline-none">
-                          Case {idx + 1}
-                        </button>
-                      )}
-                    </div>
-                  ))}
-                </div>
-              </div>
-              <div className="space-y-4">
-                <div>
-                  <div className="flex h-full w-full flex-col space-y-2">
-                    {activeTest?.test.map((assignment, idx) => (
-                      <React.Fragment key={idx}>
-                        <div className="text-label-3 dark:text-dark-label-3 text-xs font-medium">
-                          {assignment.variable} =
-                        </div>
-                        <div className="font-menlo bg-fill-3 dark:bg-dark-fill-3 w-full cursor-text rounded-lg border border-transparent px-3 py-[10px]">
-                          <div
-                            className="font-menlo placeholder:text-label-4 dark:placeholder:text-dark-label-4 sentry-unmask w-full resize-none whitespace-pre-wrap break-words outline-none"
-                            contentEditable="true"
-                          >
-                            {assignment.value}
-                          </div>
-                        </div>
-                      </React.Fragment>
-                    )) ?? null}
-                  </div>
-                </div>
-              </div>
-            </div>
+            <div className="h-full w-full bg-black">THIS IS ACTIVITY LOG</div>
           </div>
         </div>
       </div>
