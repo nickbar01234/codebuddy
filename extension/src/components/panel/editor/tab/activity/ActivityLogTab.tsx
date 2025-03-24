be16b20740e677df3d59b2969886414385995469
HUNG NGO <hungngo1607@gmail.com>
2025-03-24 18:43:18 -0400

    done ui after using some shad cn component

--- extension/src/components/panel/editor/tab/activity/ActivityLogTab.tsx
+++ extension/src/components/panel/editor/tab/activity/ActivityLogTab.tsx
@@ -2,28 +2,31 @@ import { LogEvent } from "@cb/db/converter";
 import { Send } from "lucide-react";
 import React from "react";
 import { LogEntry } from "./LogEntry";
+import { Input } from "@cb/lib/components/ui/input";
+import { Button } from "@cb/lib/components/ui/button";
+import { useWindowDimensions } from "@cb/hooks/index";
 
 interface ActivityLogProps {
   logEntries: LogEvent[];
 }
 
 export const ActivityLogTab: React.FC<ActivityLogProps> = ({ logEntries }) => {
+  const { height } = useWindowDimensions();
   return (
-    <div className="h-full w-full overflow-hidden rounded-lg p-2 shadow-md">
-      <div className="h-full w-full">
+    <div className="flex h-full w-full flex-col items-center gap-2 overflow-hidden rounded-lg p-2 shadow-md">
+      <div
+        className="flex w-full flex-col gap-2 overflow-y-auto p-2"
+        style={{ height: height - 128 }}
+      >
         {logEntries.map((entry, index) => (
           <LogEntry key={index} entry={entry} />
         ))}
       </div>
-      <div className="flex items-center bg-[--color-tab-hover-background] px-4 py-2">
-        <input
-          type="text"
-          placeholder="Enter message..."
-          className="flex-grow rounded-l-md border px-2 py-1 focus:outline-none focus:ring-2 focus:ring-blue-500"
-        />
-        <button className="rounded-r-m">
-          <Send className="h-5 w-5 text-blue-600" />
-        </button>
+      <div className="item flex w-full items-center space-x-2 bg-[--color-tab-hover-background] p-2">
+        <Input type="message" placeholder="Enter message" />
+        <Button type="submit">
+          <Send className="h-5 w-5" />
+        </Button>
       </div>
     </div>
   );
