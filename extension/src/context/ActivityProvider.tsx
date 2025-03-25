eb73531a3a9401cf4cbe2a0f6876d39623688fbd
HUNG NGO <hungngo1607@gmail.com>
2025-03-24 22:11:32 -0400

    add context

--- extension/src/context/ActivityProvider.tsx
+++ extension/src/context/ActivityProvider.tsx
@@ -1,42 +1,42 @@
-import { createContext, useContext, useEffect, useState, useRef } from "react";
-import {
-  collection,
-  query,
-  orderBy,
-  limit,
-  onSnapshot,
-} from "firebase/firestore";
-import { db } from "@cb/db";
-
+import { getRoomRef } from "@cb/db";
+import { collection, onSnapshot, orderBy, query } from "firebase/firestore";
+import { createContext, useContext, useEffect, useRef, useState } from "react";
+import { useRTC } from "../hooks";
+import { LogEvent, logEventConverter } from "../db/converter";
 interface ActivityContextProps {
   children?: React.ReactNode;
 }
 
-const ActivityContext = createContext({});
+interface ActivityContext {
+  activities: LogEvent[];
+  scroll: React.MutableRefObject<any>;
+}
+
+const ActivityContext = createContext({} as ActivityContext);
 
 export const ActivityProvider = (props: ActivityContextProps) => {
-  const [activities, setActivities] = useState([]);
+  const [activities, setActivities] = useState<LogEvent[]>([] as LogEvent[]);
   const scroll = useRef();
+  const { roomId } = useRTC();
 
   useEffect(() => {
+    if (!roomId) return;
     const q = query(
-      collection(db, "activities"),
-      orderBy("createdAt", "desc"),
-      limit(50)
-    );
+      collection(getRoomRef(roomId), "logs"),
+      orderBy("times", "desc")
+    ).withConverter(logEventConverter);
 
     const unsubscribe = onSnapshot(q, (querySnapshot) => {
       const fetchedActivities = querySnapshot.docs.map((doc) => ({
         id: doc.id,
         ...doc.data(),
-        createdAt: doc.data().createdAt?.toDate?.() || new Date(), // Handle Firestore timestamp
+        timeStamp: doc.data().timestamp,
       }));
 
       setActivities(fetchedActivities.reverse()); // Ensure oldest first
     });
-
     return () => unsubscribe();
-  }, []);
+  }, [roomId]);
 
   return (
     <ActivityContext.Provider value={{ activities, scroll }}>
