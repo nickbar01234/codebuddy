a14e6e4af423b6c9476ee79e926d64e3974a1a62
HUNG NGO <hungngo1607@gmail.com>
2025-03-25 14:37:46 -0400

    adding cloud func

--- firebase/functions/src/index.ts
+++ firebase/functions/src/index.ts
@@ -5,6 +5,7 @@ import { logger } from "firebase-functions";
 import {
   onDocumentCreated,
   onDocumentDeleted,
+  onDocumentUpdated,
 } from "firebase-functions/firestore";
 
 initializeApp();
@@ -35,3 +36,59 @@ export const setExpirationDate = onDocumentCreated(
     }
   }
 );
+
+export const onRoomUsernamesUpdate = onDocumentUpdated(
+  "rooms/{roomId}",
+  async (event) => {
+    const change = event.data;
+    const context = event;
+    if (!change) return;
+
+    const before = change.before.data();
+    const after = change.after.data();
+    const roomId = context.params.roomId;
+
+    if (!before || !after) return;
+
+    const beforeUsernames = new Set(before.usernames || []);
+    const afterUsernames = new Set(after.usernames || []);
+
+    const joinedUsers = [...afterUsernames].filter(
+      (u) => !beforeUsernames.has(u)
+    );
+    const leftUsers = [...beforeUsernames].filter(
+      (u) => !afterUsernames.has(u)
+    );
+
+    const events: any[] = [];
+
+    joinedUsers.forEach((username) => {
+      events.push({
+        type: "connection",
+        timestamp: Timestamp.now(),
+        payload: {
+          username,
+          status: "join",
+        },
+      });
+    });
+
+    leftUsers.forEach((username) => {
+      events.push({
+        type: "connection",
+        timestamp: Timestamp.now(),
+        payload: {
+          username,
+          status: "leave",
+        },
+      });
+    });
+
+    // Add events to Firestore logs
+    await Promise.all(events.map((event) => addEventToRoom(event, roomId)));
+  }
+);
+
+function addEventToRoom(event: any, roomId: string) {
+  return db.collection(`rooms/${roomId}/logs`).add(event);
+}
