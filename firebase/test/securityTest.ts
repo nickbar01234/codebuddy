834b77cb8a51ebd8fd9062e08426e3ee0deb2dc5
Anhtran0208 <anhtrannd2004@gmail.com>
2025-03-24 22:35:26 -0400

    first test

---
+++ firebase/test/securityTest.ts
@@ -0,0 +1,41 @@
+import {
+  assertFails,
+  assertSucceeds,
+  initializeTestEnvironment,
+} from "@firebase/rules-unit-testing";
+import { readFileSync } from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const rulesPath = path.resolve(__dirname, "../firestore.rules");
+
+(async () => {
+  const roomData = {
+    questionId: "two-sum",
+    expiredAt: new Date(),
+    usernames: ["code@gmail.com"],
+  };
+
+  const testEnv = await initializeTestEnvironment({
+    projectId: "demo-code-buddy-development",
+    firestore: {
+      rules: readFileSync(rulesPath, "utf-8"),
+      host: "localhost",
+      port: 3001,
+    },
+  });
+
+  const authenticatedUser = testEnv.authenticatedContext("codebuddytest", {
+    email: "code@gmail.com",
+  });
+  const db = authenticatedUser.firestore();
+  const roomDoc = db.collection("rooms").doc(`CODEBUDDYTEST ${new Date()}`);
+  console.log("Authenticated user can write from room");
+
+  await assertSucceeds(roomDoc.set(roomData));
+  console.log("Authenticated user can read from room");
+
+  await assertSucceeds(roomDoc.get());
+})();
