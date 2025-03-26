c8056ca664925fca55cc4dcac1038357cb7b1f05
HUNG NGO <hungngo1607@gmail.com>
2025-03-26 12:56:28 -0400

    add scroll to view

--- extension/src/components/panel/editor/tab/activity/ActivityLogTab.tsx
+++ extension/src/components/panel/editor/tab/activity/ActivityLogTab.tsx
@@ -1,10 +1,16 @@
 import { LogEvent } from "@cb/db/converter";
-import { useActivity, useAppState, useWindowDimensions } from "@cb/hooks/index";
+import {
+  useActivity,
+  useAppState,
+  useOnMount,
+  usePeerSelection,
+  useWindowDimensions,
+} from "@cb/hooks/index";
 import { Button } from "@cb/lib/components/ui/button";
 import { Input } from "@cb/lib/components/ui/input";
 import { Timestamp } from "firebase/firestore";
 import { Activity, Send } from "lucide-react";
-import React from "react";
+import React, { useEffect } from "react";
 import { LogEntry } from "./LogEntry";
 
 export const ActivityLogTab: React.FC = () => {
@@ -14,10 +20,40 @@ export const ActivityLogTab: React.FC = () => {
   } = useWindowDimensions();
   const [messageValue, setMessage] = React.useState("");
   const { activities, sendActivity } = useActivity();
+  const { isBuffer } = usePeerSelection();
   const {
     user: { username },
   } = useAppState();
-  const logEntries = [...dummy, ...activities];
+  const scroll = React.useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (scroll.current) {
+      scroll.current.scrollTo({
+        top: scroll.current.scrollHeight,
+        behavior: "smooth",
+      });
+    }
+  }, [activities, isBuffer]);
+
+  const onSubmit = React.useCallback(() => {
+    const userColorKeys = Object.keys(userColors);
+    const randomColor =
+      userColors[
+        userColorKeys[
+          Math.floor(Math.random() * userColorKeys.length)
+        ] as keyof typeof userColors
+      ];
+    sendActivity({
+      type: "message",
+      payload: {
+        username: username,
+        message: messageValue,
+        color: randomColor,
+      },
+    });
+    setMessage("");
+  }, [messageValue, sendActivity, username]);
+
   return (
     <div className="bg-layer-1 dark:bg-dark-layer-1 flex h-full w-full flex-col items-center gap-2 overflow-hidden rounded-lg p-4 shadow-md">
       <div className="flex w-full items-start gap-1">
@@ -27,8 +63,9 @@ export const ActivityLogTab: React.FC = () => {
       <div
         className="hide-scrollbar flex w-full flex-col gap-1 overflow-y-auto p-4"
         style={{ height: height - codePreference.height - 150 }}
+        ref={scroll}
       >
-        {logEntries.map((entry, index) => (
+        {activities.map((entry, index) => (
           <LogEntry key={index} entry={entry} />
         ))}
       </div>
@@ -38,28 +75,16 @@ export const ActivityLogTab: React.FC = () => {
           placeholder="Enter message"
           value={messageValue}
           onChange={(e) => setMessage(e.target.value)}
+          onKeyDown={(e) => {
+            if (e.key === "Enter" && messageValue.trim() !== "") {
+              onSubmit();
+            }
+          }}
         />
         <Button
           type="submit"
           className="hover:bg-[--color-tab-hover-background]"
-          onClick={() => {
-            const userColorKeys = Object.keys(userColors);
-            const randomColor =
-              userColors[
-                userColorKeys[
-                  Math.floor(Math.random() * userColorKeys.length)
-                ] as keyof typeof userColors
-              ];
-            sendActivity({
-              type: "message",
-              payload: {
-                username: username,
-                message: messageValue,
-                color: randomColor,
-              },
-            });
-            setMessage("");
-          }}
+          onClick={onSubmit}
         >
           <Send className="h-5 w-5 text-black dark:text-white" />
         </Button>
