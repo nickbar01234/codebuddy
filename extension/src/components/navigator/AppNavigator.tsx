18df62f76cc19dab755352e3fb4f6b2987cb45a2
HUNG NGO <hungngo1607@gmail.com>
2025-03-29 14:01:29 -0400

    add change

--- extension/src/components/navigator/AppNavigator.tsx
+++ extension/src/components/navigator/AppNavigator.tsx
@@ -4,21 +4,16 @@ import { LoadingPanel } from "@cb/components/panel/LoadingPanel";
 import Header from "@cb/components/ui/Header";
 import { AppState, appStateContext } from "@cb/context/AppStateProvider";
 import { usePeerSelection } from "@cb/hooks/index";
-import { usePeerSelection } from "@cb/hooks/index";
 import useDevSetupRoom from "@cb/hooks/useDevSetupRoom";
 import { getLocalStorage } from "@cb/services";
 import { getQuestionIdFromUrl } from "@cb/utils";
 import { cn } from "@cb/utils/cn";
 import React from "react";
 import { RejoinPrompt } from "./menu/RejoinPrompt";
-import { getQuestionIdFromUrl } from "@cb/utils";
 
 export const AppNavigator = () => {
   const { state } = React.useContext(appStateContext);
   const { peers, setActivePeerId } = usePeerSelection();
-  const sessionId = React.useMemo(() => {
-    return getQuestionIdFromUrl(window.location.href);
-  }, []);
   useDevSetupRoom();
 
   const currentTabInfo = getLocalStorage("tabs");
