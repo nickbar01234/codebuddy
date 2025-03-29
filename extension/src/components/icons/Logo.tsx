40f07902b241c89ce3951c639f68af01811996a4
haianhng31 <anguyen137@fordham.edu>
2025-03-29 13:36:24 -0400

    refactor logo

---
+++ extension/src/components/icons/Logo.tsx
@@ -0,0 +1,2 @@
+export const darkLogo = chrome.runtime.getURL("images/logo_dark.png");
+export const lightLogo = chrome.runtime.getURL("images/logo_light.png");
--- extension/src/components/ui/Header.tsx
+++ extension/src/components/ui/Header.tsx
@@ -1,11 +1,9 @@
 import { cn } from "@cb/utils/cn";
+import { darkLogo, lightLogo } from "../icons/Logo";
 
 interface HeaderProps extends React.HTMLAttributes<HTMLDivElement> {}
 
 const Header = ({ className, ...props }: HeaderProps) => {
-  const darkLogo = chrome.runtime.getURL("images/logo_dark.png");
-  const lightLogo = chrome.runtime.getURL("images/logo_light.png");
-
   return (
     <div className={cn("flex items-center gap-1", className)} {...props}>
       <div className="h-5 w-5">
