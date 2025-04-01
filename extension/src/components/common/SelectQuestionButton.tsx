189791260d9aeb9839a908eeb1dceb53b63826cd
HUNG NGO <hungngo1607@gmail.com>
2025-04-01 18:56:33 -0400

    add some move around file

--- extension/src/components/common/SelectQuestionButton.tsx
+++ extension/src/components/common/SelectQuestionButton.tsx
@@ -1,20 +1,20 @@
 interface SelectButtonQuestionProps {
-  href: string;
+  onClick: () => void; // Function to handle click}
 }
 
-const SelectQuestionButton = ({ href }: SelectButtonQuestionProps) => {
+export const SelectQuestionButton = ({
+  onClick,
+}: SelectButtonQuestionProps) => {
   return (
     // todo(nickbar01234) - Adding manual margin left to align the other columns. We should find a better method
     <div className="ml-7 mr-2 flex items-center py-[11px]" role="cell">
       <button
         className="rounded-md p-2 text-white"
         style={{ backgroundColor: "#DD5471" }}
-        onClick={() => console.log(href)}
+        onClick={onClick}
       >
         Select
       </button>
     </div>
   );
 };
-
-export default SelectQuestionButton;
