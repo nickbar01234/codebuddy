0631eedca655805ac1649bd3b97a6efbe3ec4f7a
haianhng31 <anguyen137@fordham.edu>
2025-04-02 19:03:20 -0400

    add ignore file dev.mjs in eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,17 +1,17 @@
 // @ts-check
 
 import eslint from "@eslint/js";
-import tseslint from "typescript-eslint";
+import tsEslintPlugin from "@typescript-eslint/eslint-plugin";
 import react from "eslint-plugin-react";
 import reactHooks from "eslint-plugin-react-hooks";
-import tsEslintPlugin from "@typescript-eslint/eslint-plugin";
 import globals from "globals";
+import tseslint from "typescript-eslint";
 
 export default tseslint.config(
   eslint.configs.recommended,
   tseslint.configs.recommended,
   {
-    ignores: ["**/node_modules/*", "**/dist/*"]
+    ignores: ["**/node_modules/*", "**/dist/*", "**/script/dev.mjs"],
   },
   {
     rules: {
@@ -42,5 +42,5 @@ export default tseslint.config(
       ...react.configs.flat.recommended.languageOptions,
       globals: globals.browser,
     },
-  },
+  }
 );
