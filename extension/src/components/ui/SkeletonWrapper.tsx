e8ed124fffdd06c5eabc6074d9154f6033cac90e
HUNG NGO <hungngo1607@gmail.com>
2025-04-04 01:09:42 -0400

    add type

--- extension/src/components/ui/SkeletonWrapper.tsx
+++ extension/src/components/ui/SkeletonWrapper.tsx
@@ -1,15 +1,15 @@
 import { Skeleton } from "@cb/lib/components/ui/skeleton";
 import { cn } from "@cb/utils/cn";
-import React from "react";
-
-export const SkeletonWrapper = ({
+import React, { FC } from "react";
+interface SkelentonWrapperProps extends React.ComponentProps<typeof Skeleton> {
+  loading: boolean;
+  children?: React.ReactNode;
+  className?: string;
+}
+export const SkeletonWrapper: FC<SkelentonWrapperProps> = ({
   children,
   className,
   loading,
-}: {
-  children: React.ReactNode;
-  className?: string;
-  loading?: boolean;
 }) => {
   return loading ? (
     <Skeleton
