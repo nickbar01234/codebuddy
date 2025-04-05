3df594c4a0c2d0944af13071b538f854b00441c9
Nick Doan <nickbar01234@gmail.com>
2025-04-04 21:55:36 -0400

    Get language extension (#223)
    * Get language extension
    
    * Fix test name
    
    * Address comments
    
    * Fix merge conflicts
    
    * Fix tests
    
    ---------
    
    Co-authored-by: nickbar01234 <nickbar01234gmail.com>

---
+++ extension/src/hooks/useLanguageExtension.ts
@@ -0,0 +1,33 @@
+import { sendServiceRequest } from "@cb/services";
+import { ServiceResponse } from "@cb/types";
+import { poll } from "@cb/utils/poll";
+import React from "react";
+import { useOnMount } from ".";
+
+const useLanguageExtension = () => {
+  const [languageExtensions, setLanguageExtensions] = React.useState<
+    ServiceResponse["getLanguageExtension"]
+  >([]);
+
+  useOnMount(() => {
+    poll({
+      fn: async () => sendServiceRequest({ action: "getLanguageExtension" }),
+      until: (response) => response instanceof Array && response.length > 0,
+    })
+      .then(setLanguageExtensions)
+      .catch(console.error);
+  });
+
+  const getLanguageExtension = React.useCallback(
+    (languageId?: string) =>
+      languageExtensions.find((language) => language.id === languageId)
+        ?.extensions[0],
+    [languageExtensions]
+  );
+
+  return {
+    getLanguageExtension,
+  };
+};
+
+export default useLanguageExtension;
