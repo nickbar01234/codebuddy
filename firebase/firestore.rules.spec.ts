1ac914be549decf9dcd3b4fbbd274deaf825b9b9
Anh Tran <66905335+Anhtran0208@users.noreply.github.com>
2025-04-08 00:00:38 -0400

    Setup firebase security (#186)
    * first test
    
    * add fail test
    
    * pull main
    
    * remove folder
    
    * add dev dependency
    
    * first setup vitest will add more later
    
    * resolve conflict
    
    * fix rules to auto join
    
    * add test and fix rules
    
    * fix rule
    
    * remove then
    
    * add inline comment
    
    * install jsdom
    
    * test docker
    
    * fix locked files
    
    * try removing
    
    * add ci/cd
    
    * remove health check
    
    * remove push
    
    * fix error in ci/cd
    
    * fix error
    
    * fix error
    
    * try to 2 tests
    
    * fix docker
    
    * wait for docker
    
    * fix docker
    
    * merge main fix docker
    
    * add firebase rules
    
    * add firebase rules
    
    * fix test
    
    * fix test
    
    * add health check
    
    * fix health check
    
    * fix health check
    
    * try use wait
    
    * debug
    
    * finally healthy
    
    * add echo
    
    * refactor testing
    
    * fix nit

--- firebase/firestore.rules.spec.ts
+++ firebase/firestore.rules.spec.ts
@@ -1,7 +1,90 @@
-import { describe, expect, it } from "vitest";
+// @vitest-environment jsdom
+import {
+  assertFails,
+  assertSucceeds,
+  initializeTestEnvironment,
+  RulesTestContext,
+  RulesTestEnvironment,
+} from "@firebase/rules-unit-testing";
+import { readFileSync } from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+import { afterAll, beforeAll, describe, it } from "vitest";
 
-describe("DemoTest", () => {
-  it("TestRunner", () => {
-    expect(1).toBe(1);
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const rulesPath = path.resolve(__dirname, "firestore.rules");
+
+describe("Firebase security test", () => {
+  let testEnv: RulesTestEnvironment;
+  const roomData = {
+    questionId: "two-sum",
+    expiredAt: new Date(),
+    usernames: ["code@gmail.com", "buddy@hotmail.com"],
+  };
+  let authenticatedUser: RulesTestContext;
+  let unauthenticatedUser: RulesTestContext;
+  let authenticatedDb: ReturnType<RulesTestContext["firestore"]>;
+  let unauthenticatedDb: ReturnType<RulesTestContext["firestore"]>;
+
+  const createRoom = (db: ReturnType<RulesTestContext["firestore"]>) => {
+    const roomId = `CODEBUDDYTEST_${Date.now()}`;
+    return db.collection("rooms").doc(roomId);
+  };
+
+  const createSession = (roomDoc: ReturnType<typeof createRoom>) => {
+    return roomDoc.collection("sessions").doc("two-sum");
+  };
+
+  beforeAll(async () => {
+    testEnv = await initializeTestEnvironment({
+      projectId: "demo-code-buddy-development",
+      firestore: {
+        rules: readFileSync(rulesPath, "utf-8"),
+        host: "localhost",
+        port: 3001,
+      },
+    });
+    authenticatedUser = testEnv.authenticatedContext("codebuddytest", {
+      email: "code@gmail.com",
+    });
+    unauthenticatedUser = testEnv.unauthenticatedContext();
+    authenticatedDb = authenticatedUser.firestore();
+    unauthenticatedDb = unauthenticatedUser.firestore();
+  });
+
+  afterAll(async () => {
+    await testEnv.cleanup();
+  });
+
+  it("should allow authenticated users to read/write rooms", async () => {
+    const authenticatedRoomDoc = createRoom(authenticatedDb);
+    const authenticatedSession = createSession(authenticatedRoomDoc);
+    await assertSucceeds(authenticatedRoomDoc.set(roomData));
+    await assertSucceeds(authenticatedRoomDoc.get());
+    await assertSucceeds(authenticatedSession.set({}));
+    await assertFails(authenticatedRoomDoc.update({ expiredAt: new Date() }));
+  });
+
+  it("should deny unauthenticated users from read/write rooms", async () => {
+    const unauthenticatedRoomDoc = createRoom(unauthenticatedDb);
+    await assertFails(unauthenticatedRoomDoc.set(roomData));
+    await assertFails(unauthenticatedRoomDoc.get());
+  });
+
+  it("only user in usernames array can access session document", async () => {
+    const anotherEmail = testEnv.authenticatedContext("anotherUser", {
+      email: "another@gmail.com",
+    });
+    const invalidRoomDoc = createRoom(anotherEmail.firestore());
+    const invalidSessionRef = createSession(invalidRoomDoc);
+    await assertFails(invalidSessionRef.set({}));
+  });
+
+  it("should deny random path", async () => {
+    const randomCollection = authenticatedDb
+      .collection("randomCollection")
+      .doc("randomDoc");
+    await assertFails(randomCollection.set(roomData));
   });
 });
