71dffdf110d586a41ed77017562d78282158a69a
nickbar01234 <nickbar01234gmail.com>
2025-04-13 00:57:36 -0400

    Remove unused variables

--- extension/src/context/RTCProvider.tsx
+++ extension/src/context/RTCProvider.tsx
@@ -83,7 +83,6 @@ export interface RTCContext {
   joinRoom: (roomId: string) => Promise<boolean>;
   leaveRoom: (roomId: string | null) => Promise<void>;
   roomId: string | null;
-  sessionId: string;
   setRoomId: (id: string) => void;
   informations: Record<string, PeerInformation>;
   peerState: Record<string, PeerState>;
@@ -95,14 +94,6 @@ export interface RTCContext {
 
 interface RTCProviderProps {
   children: React.ReactNode;
-}
-
-export enum RoomState {
-  CODE,
-  CHOOSE,
-  WAIT,
-  DECISION,
-  NAVIGATE,
 }
 
 export const RTCContext = React.createContext({} as RTCContext);
@@ -896,7 +887,6 @@ export const RTCProvider = (props: RTCProviderProps) => {
         informations,
         peerState,
         joiningBackRoom,
-        sessionId,
         handleChooseQuestion,
         chooseQuestion,
         handleNavigateToNextQuestion,
