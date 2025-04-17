b47b9ce6cfff7fdfc5d4dc705a6b00fc7d7ec26c
HUNG NGO <hungngo1607@gmail.com>
2025-04-17 16:34:55 -0400

    add

--- extension/src/components/dialog/LeaveRoomDialog.tsx
+++ extension/src/components/dialog/LeaveRoomDialog.tsx
@@ -1,21 +1,16 @@
 import { AppState } from "@cb/context/AppStateProvider";
 import { useAppState, useRTC } from "@cb/hooks/index";
-import {
-  Dialog,
-  DialogClose,
-  DialogContent,
-  DialogDescription,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from "@cb/lib/components/ui/dialog";
+import { Button } from "@cb/lib/components/ui/button";
+import { DialogClose } from "@cb/lib/components/ui/dialog";
 import { throttle } from "lodash";
 import React, { ReactNode } from "react";
+import { RoomDialog } from "./RoomDialog";
 
 interface LeaveRoomDialogProps {
   trigger: ReactNode;
 }
 
+const baseButtonClassName = "";
 export function LeaveRoomDialog({ trigger }: LeaveRoomDialogProps) {
   const { roomId, leaveRoom } = useRTC();
   const { setState: setAppState } = useAppState();
@@ -31,34 +26,23 @@ export function LeaveRoomDialog({ trigger }: LeaveRoomDialogProps) {
   }, [roomId, leaveRoom, setAppState]);
 
   return (
-    <Dialog>
-      <DialogTrigger asChild>{trigger}</DialogTrigger>
-      <DialogContent>
-        <DialogHeader>
-          <DialogTitle className="text-left text-xl">
-            Are you sure that you want to leave the room?
-          </DialogTitle>
-          <DialogDescription className="text-left font-medium">
-            You will be disconnected, and you may not be able to rejoin unless
-            invited again.
-          </DialogDescription>
-          <div className="mt-4 flex w-full items-center justify-end gap-2 self-end">
-            <DialogClose asChild>
-              <button
-                className="hover:bg-fill-secondary h-10 rounded-md px-4 py-2"
-                onClick={leaveRoomThrottled}
-              >
-                <span className="text-sm font-medium">Yes</span>
-              </button>
-            </DialogClose>
-            <DialogClose asChild>
-              <button className="h-10 rounded-md px-4 py-2 hover:bg-[--color-tab-hover-background]">
-                <span className="text-sm font-medium">No</span>
-              </button>
-            </DialogClose>
-          </div>
-        </DialogHeader>
-      </DialogContent>
-    </Dialog>
+    <RoomDialog
+      trigger={trigger}
+      title="Are you sure that you want to leave the room?"
+      description="You will be disconnected, and you may not be able to rejoin unless invited again."
+    >
+      <div className="flex w-full items-center justify-end gap-2 self-end">
+        <DialogClose asChild>
+          <Button className={baseButtonClassName} onClick={leaveRoomThrottled}>
+            <span className="text-sm font-medium">Yes</span>
+          </Button>
+        </DialogClose>
+        <DialogClose asChild>
+          <Button className={baseButtonClassName}>
+            <span className="text-sm font-medium">No</span>
+          </Button>
+        </DialogClose>
+      </div>
+    </RoomDialog>
   );
 }
