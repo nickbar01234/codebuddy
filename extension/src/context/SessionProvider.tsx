0b262a1d540b969c1eb69bb68768089a57259c6e
HUNG NGO <hungngo1607@gmail.com>
2025-04-20 13:44:24 -0400

    fix again some

--- extension/src/context/SessionProvider.tsx
+++ extension/src/context/SessionProvider.tsx
@@ -1,8 +1,18 @@
+import { auth } from "@cb/db";
 import { useOnMount } from "@cb/hooks";
-import useAuthenticate from "@cb/hooks/useAuthenticate";
+import { getLocalStorage } from "@cb/services";
 import { initialAuthenticateCheck } from "@cb/state/session/sessionSlice";
 import { AuthenticationStatus, Status } from "@cb/types";
+import { poll } from "@cb/utils/poll";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  Unsubscribe,
+} from "firebase/auth/web-extension";
+import lodash from "lodash";
 import React from "react";
+
+const AUTHENTICATION_DELAY = 2000;
 interface SessionProviderProps {
   children?: React.ReactNode;
 }
@@ -19,9 +29,101 @@ const SessionProvider = (props: SessionProviderProps) => {
   const { children } = props;
   const [authenticationStatus, setAuthenticationStatus] =
     React.useState<AuthenticationStatus>({ status: Status.LOADING });
+  const unsubscribeRef = React.useRef<Unsubscribe>();
 
-  useAuthenticate({
-    authenticate: setAuthenticationStatus,
+  useOnMount(() => {
+    if (import.meta.env.MODE !== "development") {
+      return;
+    }
+
+    const selfAuthenticate = async () => {
+      const user = await poll({
+        fn: async () => getLocalStorage("test"),
+        until: (x) => x != undefined,
+      });
+      if (user != undefined) {
+        const { peer } = user;
+        createUserWithEmailAndPassword(auth, peer, "TEST_PASSWORD")
+          .catch((error) => {
+            if (error.code !== "auth/email-already-in-use") {
+              console.error(error);
+            }
+          })
+          .finally(() =>
+            signInWithEmailAndPassword(auth, peer, "TEST_PASSWORD")
+          );
+      }
+    };
+
+    selfAuthenticate();
+  });
+
+  useOnMount(() => {
+    lodash.delay(() => {
+      unsubscribeRef.current = auth.onAuthStateChanged((user) => {
+        if (user == null) {
+          setAuthenticationStatus({ status: Status.UNAUTHENTICATED });
+        } else {
+          setAuthenticationStatus({
+            status: Status.AUTHENTICATED,
+            user: { username: user.displayName ?? user.email! },
+          });
+        }
+      });
+    }, AUTHENTICATION_DELAY);
+
+    return () => {
+      if (unsubscribeRef.current) {
+        unsubscribeRef.current();
+      }
+    };
+  });
+  useOnMount(() => {
+    if (import.meta.env.MODE !== "development") {
+      return;
+    }
+
+    const selfAuthenticate = async () => {
+      const user = await poll({
+        fn: async () => getLocalStorage("test"),
+        until: (x) => x != undefined,
+      });
+      if (user != undefined) {
+        const { peer } = user;
+        createUserWithEmailAndPassword(auth, peer, "TEST_PASSWORD")
+          .catch((error) => {
+            if (error.code !== "auth/email-already-in-use") {
+              console.error(error);
+            }
+          })
+          .finally(() =>
+            signInWithEmailAndPassword(auth, peer, "TEST_PASSWORD")
+          );
+      }
+    };
+
+    selfAuthenticate();
+  });
+
+  useOnMount(() => {
+    lodash.delay(() => {
+      unsubscribeRef.current = auth.onAuthStateChanged((user) => {
+        if (user == null) {
+          setAuthenticationStatus({ status: Status.UNAUTHENTICATED });
+        } else {
+          setAuthenticationStatus({
+            status: Status.AUTHENTICATED,
+            user: { username: user.displayName ?? user.email! },
+          });
+        }
+      });
+    }, AUTHENTICATION_DELAY);
+
+    return () => {
+      if (unsubscribeRef.current) {
+        unsubscribeRef.current();
+      }
+    };
   });
 
   useOnMount(() => {
