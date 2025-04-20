455f36dc4f104dbe4b29697d1ed54861ea9d06b3
Anhtran0208 <anhtrannd2004@gmail.com>
2025-04-20 01:12:39 -0400

    fix scroll

--- extension/src/components/panel/editor/tab/activity/ActivityLogTab.tsx
+++ extension/src/components/panel/editor/tab/activity/ActivityLogTab.tsx
@@ -13,21 +13,26 @@ export const ActivityLogTab: React.FC<ActivityLogTabProps> = ({
   logEntries,
 }) => {
   return (
-    <div className="bg-layer-1 dark:bg-dark-layer-1 flex h-full w-full flex-col gap-2 rounded-lg p-4 shadow-md">
-      <div className="hide-scrollbar flex w-full justify-start flex-col flex-1 gap-1 overflow-y-auto p-4">
-        {logEntries.map((entry, index) => (
-          <LogEntry key={index} entry={entry} />
-        ))}
-      </div>
-      <div className="mb-2 w-full overflow-x-auto overflow-y-hidden">
-        <div className="flex min-w-max items-center">
-          <Input type="message" placeholder="Enter message" />
-          <Button
-            type="submit"
-            className="hover:bg-[--color-tab-hover-background]"
-          >
-            <Send className="h-5 w-5 text-black dark:text-white" />
-          </Button>
+    <div className="bg-layer-1 dark:bg-dark-layer-1 flex h-full w-full flex-col rounded-lg p-4 shadow-md overflow-hidden">
+      <div className="flex-1 w-full overflow-x-auto">
+        <div className="min-w-max flex flex-col h-full">
+          <div className="hide-scrollbar flex-1 overflow-y-auto p-4 flex flex-col gap-1">
+            {logEntries.map((entry, index) => (
+              <LogEntry key={index} entry={entry} />
+            ))}
+          </div>
+
+          <div className="mb-2 w-full overflow-x-auto overflow-y-hidden">
+            <div className="flex min-w-max items-center">
+              <Input type="message" placeholder="Enter message" />
+              <Button
+                type="submit"
+                className="hover:bg-[--color-tab-hover-background]"
+              >
+                <Send className="h-5 w-5 text-black dark:text-white" />
+              </Button>
+            </div>
+          </div>
         </div>
       </div>
     </div>
