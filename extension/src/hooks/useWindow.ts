5effc0520ffb76a781555d2e2729f5af34fac79a
Long Dau <dtnlong04@gmail.com>
2025-04-22 00:11:18 -0500

    first pass

---
+++ extension/src/hooks/useWindow.ts
@@ -0,0 +1,39 @@
+import {
+  setAppWidth as _setAppWidth,
+  setCodePreferenceHeight as _setCodePreferenceHeight,
+} from "@cb/state/slices/windowSlice";
+import type { AppDispatch, RootState } from "@cb/state/store";
+import {
+  savePreferenceNow,
+  toggleWidthAndSave,
+} from "@cb/state/thunks/windowThunks";
+import { useDispatch, useSelector } from "react-redux";
+
+export function useWindow() {
+  const dispatch = useDispatch<AppDispatch>();
+  const { width, height, preference } = useSelector(
+    (state: RootState) => state.window
+  );
+
+  return {
+    width,
+    height,
+    preference,
+
+    setAppWidth: (w: number) => {
+      dispatch(_setAppWidth(w));
+    },
+
+    setCodePreferenceHeight: (h: number) => {
+      dispatch(_setCodePreferenceHeight(h));
+    },
+
+    toggleWidth: () => {
+      dispatch(toggleWidthAndSave());
+    },
+
+    onResizeStop: () => {
+      dispatch(savePreferenceNow());
+    },
+  };
+}
