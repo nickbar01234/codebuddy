a2eb23bf931e874185f0a0c09ce7269acf9b8a40
HUNG NGO <hungngo1607@gmail.com>
2025-04-26 14:37:02 -0400

    add

--- extension/src/components/navigator/AppNavigator.tsx
+++ extension/src/components/navigator/AppNavigator.tsx
@@ -1,10 +1,12 @@
-import { RoomControlMenu } from "@cb/components/navigator/menu/RoomControlMenu";
-import Header from "@cb/components/ui/Header";
-import { appStateContext } from "@cb/context/AppStateProvider";
+import { RejoinPromptDialog } from "@cb/components/dialog/RejoinPromptDialog";
+import EditorPanel from "@cb/components/panel/editor";
+import HomePanel from "@cb/components/panel/HomePanel";
+import { LoadingPanel } from "@cb/components/panel/LoadingPanel";
+import { AppState, appStateContext } from "@cb/context/AppStateProvider";
 import useDevSetupRoom from "@cb/hooks/useDevSetupRoom";
 import { getLocalStorage } from "@cb/services";
+import { getQuestionIdFromUrl } from "@cb/utils";
 import React from "react";
-import { QuestionSelectorPanel } from "@cb/components/panel";
 
 export const AppNavigator = () => {
   const { state } = React.useContext(appStateContext);
@@ -13,36 +15,26 @@ export const AppNavigator = () => {
   const currentTabInfo = getLocalStorage("tabs");
 
   return (
-    <div className="flex h-full w-full flex-col">
-      <div className="hide-scrollbar flex h-9 w-full items-center justify-between gap-2 overflow-y-hidden overflow-x-scroll rounded-t-lg bg-[--color-tabset-tabbar-background] p-2">
-        <Header />
-        <RoomControlMenu />
+    <div className="relative h-full w-full overflow-hidden">
+      <div className="absolute inset-0 flex h-full w-full items-center justify-center mx-2">
+        {state === AppState.LOADING ? (
+          <LoadingPanel
+            numberOfUsers={
+              Object.keys(
+                currentTabInfo?.sessions[
+                  getQuestionIdFromUrl(window.location.href)
+                ]?.peers ?? {}
+              ).length
+            }
+          />
+        ) : state === AppState.REJOINING ? (
+          <RejoinPromptDialog />
+        ) : state === AppState.HOME ? (
+          <HomePanel />
+        ) : null}
       </div>
-      <div className="relative h-full w-full overflow-hidden">
-        <QuestionSelectorPanel
-          handleQuestionSelect={() => {}}
-          filterQuestionIds={[]}
-        />
-        {/* <div className="absolute inset-0 flex h-full w-full items-center justify-center mx-2">
-          {state === AppState.LOADING ? (
-            <LoadingPanel
-              numberOfUsers={
-                Object.keys(
-                  currentTabInfo?.sessions[
-                    getQuestionIdFromUrl(window.location.href)
-                  ]?.peers ?? {}
-                ).length
-              }
-            />
-          ) : state === AppState.REJOINING ? (
-            <RejoinPromptDialog />
-          ) : state === AppState.HOME ? (
-            <HomePanel />
-          ) : null}
-        </div>
 
-        <EditorPanel /> */}
-      </div>
+      <EditorPanel />
     </div>
   );
 };
