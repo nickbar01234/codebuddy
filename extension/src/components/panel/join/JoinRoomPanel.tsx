e6026f015bc23b2c32035716ecadaeaff0bf6faa
Van Nguyen <87792873+yourvivian@users.noreply.github.com>
2025-04-25 17:14:42 -0700

    Use mock data

--- extension/src/components/panel/join/JoinRoomPanel.tsx
+++ extension/src/components/panel/join/JoinRoomPanel.tsx
@@ -6,9 +6,10 @@ import { useRTC } from "@cb/hooks/index";
 import { Button } from "@cb/lib/components/ui/button";
 import { cn } from "@cb/utils/cn";
 import React, { useContext, useEffect, useState } from "react";
+import { sampleRooms } from "./sampleRooms.ts";
 
 // Define the Room type
-interface Room {
+export interface Room {
   id: string;
   name: string;
   currentProblem: string;
@@ -27,183 +28,6 @@ const JoinRoomPanel: React.FC = () => {
 
   useEffect(() => {
     // Use sample data for now
-    const sampleRooms: Room[] = [
-      {
-        id: "1",
-        name: "Rahhhhhh",
-        currentProblem: "3. Longest Substring Without Repeating Characters",
-        difficulty: "Medium",
-        users: 1,
-        timeElapsed: 1342, // in seconds
-      },
-      {
-        id: "2",
-        name: "LeetCodeGenius",
-        currentProblem: "1. Two Sum",
-        difficulty: "Easy",
-        users: 3,
-        timeElapsed: 3791,
-      },
-      {
-        id: "3",
-        name: "HardCoreCoders",
-        currentProblem:
-          "1420. Build Array Where You Can Find The Maximum Exactly K Comparisons",
-        difficulty: "Hard",
-        users: 2,
-        timeElapsed: 302,
-      },
-      {
-        id: "4",
-        name: "Rahhhhhh",
-        currentProblem: "3. Longest Substring Without Repeating Characters",
-        difficulty: "Medium",
-        users: 1,
-        timeElapsed: 1342, // in seconds
-      },
-      {
-        id: "5",
-        name: "LeetCodeGenius",
-        currentProblem: "1. Two Sum",
-        difficulty: "Easy",
-        users: 3,
-        timeElapsed: 3791,
-      },
-      {
-        id: "6",
-        name: "HardCoreCoders",
-        currentProblem:
-          "1420. Build Array Where You Can Find The Maximum Exactly K Comparisons",
-        difficulty: "Hard",
-        users: 2,
-        timeElapsed: 302,
-      },
-      {
-        id: "7",
-        name: "Rahhhhhh",
-        currentProblem: "3. Longest Substring Without Repeating Characters",
-        difficulty: "Medium",
-        users: 1,
-        timeElapsed: 1342, // in seconds
-      },
-      {
-        id: "8",
-        name: "LeetCodeGenius",
-        currentProblem: "1. Two Sum",
-        difficulty: "Easy",
-        users: 3,
-        timeElapsed: 3791,
-      },
-      {
-        id: "9",
-        name: "HardCoreCoders",
-        currentProblem:
-          "1420. Build Array Where You Can Find The Maximum Exactly K Comparisons",
-        difficulty: "Hard",
-        users: 2,
-        timeElapsed: 302,
-      },
-      {
-        id: "10",
-        name: "Rahhhhhh",
-        currentProblem: "3. Longest Substring Without Repeating Characters",
-        difficulty: "Medium",
-        users: 1,
-        timeElapsed: 1342, // in seconds
-      },
-      {
-        id: "11",
-        name: "LeetCodeGenius",
-        currentProblem: "1. Two Sum",
-        difficulty: "Easy",
-        users: 3,
-        timeElapsed: 3791,
-      },
-      {
-        id: "12",
-        name: "HardCoreCoders",
-        currentProblem:
-          "1420. Build Array Where You Can Find The Maximum Exactly K Comparisons",
-        difficulty: "Hard",
-        users: 2,
-        timeElapsed: 302,
-      },
-      {
-        id: "13",
-        name: "Rahhhhhh",
-        currentProblem: "3. Longest Substring Without Repeating Characters",
-        difficulty: "Medium",
-        users: 1,
-        timeElapsed: 1342, // in seconds
-      },
-      {
-        id: "14",
-        name: "LeetCodeGenius",
-        currentProblem: "1. Two Sum",
-        difficulty: "Easy",
-        users: 3,
-        timeElapsed: 3791,
-      },
-      {
-        id: "15",
-        name: "HardCoreCoders",
-        currentProblem:
-          "1420. Build Array Where You Can Find The Maximum Exactly K Comparisons",
-        difficulty: "Hard",
-        users: 2,
-        timeElapsed: 302,
-      },
-      {
-        id: "16",
-        name: "Rahhhhhh",
-        currentProblem: "3. Longest Substring Without Repeating Characters",
-        difficulty: "Medium",
-        users: 1,
-        timeElapsed: 1342, // in seconds
-      },
-      {
-        id: "17",
-        name: "LeetCodeGenius",
-        currentProblem: "1. Two Sum",
-        difficulty: "Easy",
-        users: 3,
-        timeElapsed: 3791,
-      },
-      {
-        id: "18",
-        name: "HardCoreCoders",
-        currentProblem:
-          "1420. Build Array Where You Can Find The Maximum Exactly K Comparisons",
-        difficulty: "Hard",
-        users: 2,
-        timeElapsed: 302,
-      },
-      {
-        id: "19",
-        name: "Rahhhhhh",
-        currentProblem: "3. Longest Substring Without Repeating Characters",
-        difficulty: "Medium",
-        users: 1,
-        timeElapsed: 1342, // in seconds
-      },
-      {
-        id: "20",
-        name: "LeetCodeGenius",
-        currentProblem: "1. Two Sum",
-        difficulty: "Easy",
-        users: 3,
-        timeElapsed: 3791,
-      },
-      {
-        id: "21",
-        name: "HardCoreCoders",
-        currentProblem:
-          "1420. Build Array Where You Can Find The Maximum Exactly K Comparisons",
-        difficulty: "Hard",
-        users: 2,
-        timeElapsed: 302,
-      },
-    ];
     setPublicRooms(sampleRooms);
   }, []);
 
@@ -225,7 +49,7 @@ const JoinRoomPanel: React.FC = () => {
         <JoinPrivateRoom />
 
         <div className="flex flex-col gap-4 w-full items-center">
-          <h2 className="text-xl font-semibold text-center">
+          <h2 className="text-xl font-medium text-center">
             Browse public rooms
           </h2>
 
---
+++ extension/src/components/panel/join/sampleRooms.ts
@@ -0,0 +1,179 @@
+import { Room } from "./JoinRoomPanel";
+
+export const sampleRooms: Room[] = [
+  {
+    id: "1",
+    name: "Rahhhhhh",
+    currentProblem: "3. Longest Substring Without Repeating Characters",
+    difficulty: "Medium",
+    users: 1,
+    timeElapsed: 1342, // in seconds
+  },
+  {
+    id: "2",
+    name: "LeetCodeGenius",
+    currentProblem: "1. Two Sum",
+    difficulty: "Easy",
+    users: 3,
+    timeElapsed: 3791,
+  },
+  {
+    id: "3",
+    name: "HardCoreCoders",
+    currentProblem:
+      "1420. Build Array Where You Can Find The Maximum Exactly K Comparisons",
+    difficulty: "Hard",
+    users: 2,
+    timeElapsed: 302,
+  },
+  {
+    id: "4",
+    name: "Rahhhhhh",
+    currentProblem: "3. Longest Substring Without Repeating Characters",
+    difficulty: "Medium",
+    users: 1,
+    timeElapsed: 1342, // in seconds
+  },
+  {
+    id: "5",
+    name: "LeetCodeGenius",
+    currentProblem: "1. Two Sum",
+    difficulty: "Easy",
+    users: 3,
+    timeElapsed: 3791,
+  },
+  {
+    id: "6",
+    name: "HardCoreCoders",
+    currentProblem:
+      "1420. Build Array Where You Can Find The Maximum Exactly K Comparisons",
+    difficulty: "Hard",
+    users: 2,
+    timeElapsed: 302,
+  },
+  {
+    id: "7",
+    name: "Rahhhhhh",
+    currentProblem: "3. Longest Substring Without Repeating Characters",
+    difficulty: "Medium",
+    users: 1,
+    timeElapsed: 1342, // in seconds
+  },
+  {
+    id: "8",
+    name: "LeetCodeGenius",
+    currentProblem: "1. Two Sum",
+    difficulty: "Easy",
+    users: 3,
+    timeElapsed: 3791,
+  },
+  {
+    id: "9",
+    name: "HardCoreCoders",
+    currentProblem:
+      "1420. Build Array Where You Can Find The Maximum Exactly K Comparisons",
+    difficulty: "Hard",
+    users: 2,
+    timeElapsed: 302,
+  },
+  {
+    id: "10",
+    name: "Rahhhhhh",
+    currentProblem: "3. Longest Substring Without Repeating Characters",
+    difficulty: "Medium",
+    users: 1,
+    timeElapsed: 1342, // in seconds
+  },
+  {
+    id: "11",
+    name: "LeetCodeGenius",
+    currentProblem: "1. Two Sum",
+    difficulty: "Easy",
+    users: 3,
+    timeElapsed: 3791,
+  },
+  {
+    id: "12",
+    name: "HardCoreCoders",
+    currentProblem:
+      "1420. Build Array Where You Can Find The Maximum Exactly K Comparisons",
+    difficulty: "Hard",
+    users: 2,
+    timeElapsed: 302,
+  },
+  {
+    id: "13",
+    name: "Rahhhhhh",
+    currentProblem: "3. Longest Substring Without Repeating Characters",
+    difficulty: "Medium",
+    users: 1,
+    timeElapsed: 1342, // in seconds
+  },
+  {
+    id: "14",
+    name: "LeetCodeGenius",
+    currentProblem: "1. Two Sum",
+    difficulty: "Easy",
+    users: 3,
+    timeElapsed: 3791,
+  },
+  {
+    id: "15",
+    name: "HardCoreCoders",
+    currentProblem:
+      "1420. Build Array Where You Can Find The Maximum Exactly K Comparisons",
+    difficulty: "Hard",
+    users: 2,
+    timeElapsed: 302,
+  },
+  {
+    id: "16",
+    name: "Rahhhhhh",
+    currentProblem: "3. Longest Substring Without Repeating Characters",
+    difficulty: "Medium",
+    users: 1,
+    timeElapsed: 1342, // in seconds
+  },
+  {
+    id: "17",
+    name: "LeetCodeGenius",
+    currentProblem: "1. Two Sum",
+    difficulty: "Easy",
+    users: 3,
+    timeElapsed: 3791,
+  },
+  {
+    id: "18",
+    name: "HardCoreCoders",
+    currentProblem:
+      "1420. Build Array Where You Can Find The Maximum Exactly K Comparisons",
+    difficulty: "Hard",
+    users: 2,
+    timeElapsed: 302,
+  },
+  {
+    id: "19",
+    name: "Rahhhhhh",
+    currentProblem: "3. Longest Substring Without Repeating Characters",
+    difficulty: "Medium",
+    users: 1,
+    timeElapsed: 1342, // in seconds
+  },
+  {
+    id: "20",
+    name: "LeetCodeGenius",
+    currentProblem: "1. Two Sum",
+    difficulty: "Easy",
+    users: 3,
+    timeElapsed: 3791,
+  },
+  {
+    id: "21",
+    name: "HardCoreCoders",
+    currentProblem:
+      "1420. Build Array Where You Can Find The Maximum Exactly K Comparisons",
+    difficulty: "Hard",
+    users: 2,
+    timeElapsed: 302,
+  },
+];
