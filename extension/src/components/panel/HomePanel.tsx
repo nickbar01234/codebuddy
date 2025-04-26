b8629431250e4e06b5635e8e2c60f4f69b1cf1ec
Nick Doan <nickbar01234@gmail.com>
2025-04-26 18:01:56 -0400

    Use same layout (#289)
    Co-authored-by: nickbar01234 <nickbar01234gmail.com>

--- extension/src/components/panel/HomePanel.tsx
+++ extension/src/components/panel/HomePanel.tsx
@@ -1,27 +1,15 @@
 import { CreateRoomDialog } from "@cb/components/dialog/CreateRoomDialog";
 import { JoinRoomDialog } from "@cb/components/dialog/JoinRoomDialog";
-import { ThemeAwaredLogo } from "@cb/components/icons/Logo";
+import { DefaultPanel } from "./DefaultPanel";
 
 const HomePanel = () => {
   return (
-    <div className="hide-scrollbar flex h-full w-full flex-col gap-10 overflow-x-auto justify-center mr-4">
-      <div className="flex min-w-max flex-col items-center justify-end gap-3">
-        <ThemeAwaredLogo
-          containerProps={{
-            className:
-              "aspect-square md:h-[140px] sm:h-[100px] md:w-[140px] sm:w-[100px]",
-          }}
-        />
-        <h1 className="text-2xl">
-          Code<span className="text-pinkish-red">Buddy</span>
-        </h1>
-      </div>
-
+    <DefaultPanel>
       <div className="flex min-w-max flex-col items-center gap-3">
         <CreateRoomDialog />
         <JoinRoomDialog />
       </div>
-    </div>
+    </DefaultPanel>
   );
 };
 
