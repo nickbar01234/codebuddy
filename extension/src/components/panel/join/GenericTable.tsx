a6de6c232f230308562a51b2a778ff60563cd68b
Van Nguyen <87792873+yourvivian@users.noreply.github.com>
2025-04-28 19:46:27 -0700

    Update map key values

--- extension/src/components/panel/join/GenericTable.tsx
+++ extension/src/components/panel/join/GenericTable.tsx
@@ -9,29 +9,37 @@ import {
 } from "@cb/lib/components/ui/table";
 import React from "react";
 
-interface Props<T> {
+interface IdentifiableObject {
+  id: string | number;
+}
+
+interface Props<T extends IdentifiableObject> {
   data: T[];
   headers: string[];
   renderRow: (item: T) => React.ReactNode;
 }
 
-const GenericTable = <T,>({ data, headers, renderRow }: Props<T>) => {
+const GenericTable = <T extends IdentifiableObject>({
+  data,
+  headers,
+  renderRow,
+}: Props<T>) => {
   return (
     <ScrollArea className="grow h-10 z-10">
       <SkeletonWrapper loading={data === undefined}>
         <Table className="min-w-full dark:bg-dark-layer-bg dark:text-gray-100">
           <TableHeader className="sticky top-0 bg-white dark:bg-dark-layer-bg dark:text-gray-300 z-20">
             <TableRow>
-              {headers.map((header, index) => (
-                <TableHead key={index} className="align-bottom p-2 truncate">
+              {headers.map((header) => (
+                <TableHead key={header} className="align-bottom p-2 truncate">
                   {header}
                 </TableHead>
               ))}
             </TableRow>
           </TableHeader>
           <TableBody>
-            {data.map((item, index) => (
-              <React.Fragment key={index}>{renderRow(item)}</React.Fragment>
+            {data.map((item) => (
+              <React.Fragment key={item.id}>{renderRow(item)}</React.Fragment>
             ))}
           </TableBody>
         </Table>
