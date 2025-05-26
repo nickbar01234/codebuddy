30e165acd1746958d059e30f113bef1c6de3fae6
haianhng31 <anguyen137@fordham.edu>
2025-05-26 01:17:06 -0400

    undo edits on AppNavigator

--- extension/src/components/navigator/AppNavigator.tsx
+++ extension/src/components/navigator/AppNavigator.tsx
@@ -1,6 +1,8 @@
+import { RejoinPromptDialog } from "@cb/components/dialog/RejoinPromptDialog";
 import EditorPanel from "@cb/components/panel/editor";
+import HomePanel from "@cb/components/panel/HomePanel";
 import { LoadingPanel } from "@cb/components/panel/LoadingPanel";
-import { appStateContext } from "@cb/context/AppStateProvider";
+import { AppState, appStateContext } from "@cb/context/AppStateProvider";
 import useDevSetupRoom from "@cb/hooks/useDevSetupRoom";
 import { getLocalStorage } from "@cb/services";
 import { getSessionId } from "@cb/utils";
@@ -15,12 +17,18 @@ export const AppNavigator = () => {
   return (
     <div className="relative h-full w-full overflow-hidden bg-secondary">
       <div className="absolute inset-0 flex h-full w-full items-center justify-center mx-2">
-        <LoadingPanel
-          numberOfUsers={
-            Object.keys(currentTabInfo?.sessions[getSessionId()]?.peers ?? {})
-              .length
-          }
-        />
+        {state === AppState.LOADING ? (
+          <LoadingPanel
+            numberOfUsers={
+              Object.keys(currentTabInfo?.sessions[getSessionId()]?.peers ?? {})
+                .length
+            }
+          />
+        ) : state === AppState.REJOINING ? (
+          <RejoinPromptDialog />
+        ) : state === AppState.HOME ? (
+          <HomePanel />
+        ) : null}
       </div>
 
       <EditorPanel />
