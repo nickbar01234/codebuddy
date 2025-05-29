b12652408030d215bf07e0796d8dd62464433641
HUNG NGO <hungngo1607@gmail.com>
2025-05-28 21:14:40 -0700

    add

--- extension/src/hooks/index.tsx
+++ extension/src/hooks/index.tsx
@@ -1,6 +1,7 @@
 import { appStateContext } from "@cb/context/AppStateProvider";
 import { PeerSelectionContext } from "@cb/context/PeerSelectionProvider";
 import { RTCContext } from "@cb/context/RTCProvider";
+import { sessionContext } from "@cb/context/SessionProvider";
 import { windowContext } from "@cb/context/WindowProvider";
 import { AppDispatch, RootState } from "@cb/state/store";
 import React from "react";
@@ -16,8 +17,7 @@ export const usePeerSelection = () => React.useContext(PeerSelectionContext);
 
 export const useAppState = () => React.useContext(appStateContext);
 
-export const useSession = () => useAppSelector((state) => state.session);
-
+export const useSession = () => React.useContext(sessionContext);
 export const useWindowDimensions = () => React.useContext(windowContext);
 
 export { useFirebaseListener } from "./useFirebaseListener";
