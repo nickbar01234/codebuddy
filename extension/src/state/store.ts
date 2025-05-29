b0a401dd5927a1752e92efdd955a3bebb3a30b3d
HUNG NGO <hungngo1607@gmail.com>
2025-05-28 22:25:22 -0700

    adding dev port , pending automate devtool command

--- extension/src/state/store.ts
+++ extension/src/state/store.ts
@@ -1,22 +1,27 @@
+import { DEV_PORT } from "@cb/constants/dev-port";
+import { getLocalStorage } from "@cb/services";
 import { devToolsEnhancer } from "@redux-devtools/remote";
 import { configureStore } from "@reduxjs/toolkit";
 import counterReducer from "./session/counterSlice";
+
+const user = getLocalStorage("test");
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
   },
   enhancers: (defaultEnhancers) =>
-    process.env.NODE_ENV === "development"
-      ? defaultEnhancers().concat(
-          devToolsEnhancer({
-            name: "CodeBuddy",
-            realtime: true,
-            hostname: "localhost",
-            port: 8001,
-          })
-        )
-      : defaultEnhancers(),
+    defaultEnhancers().concat(
+      devToolsEnhancer({
+        name: user?.peer ?? "CodeBuddy Dev",
+        realtime: true,
+        hostname: "localhost",
+        port:
+          user?.peer && user?.peer in DEV_PORT
+            ? DEV_PORT[user.peer as keyof typeof DEV_PORT]
+            : 8000,
+      })
+    ),
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck: false,
