5e391839c1c1de879fd1628f48bda82b4fce95ad
HUNG NGO <hungngo1607@gmail.com>
2025-06-07 14:13:14 -0700

    add

--- extension/dev.sh
+++ extension/dev.sh
@@ -2,5 +2,4 @@ redux-devtools --hostname=localhost --port=8000 --open --name='Code' &
 redux-devtools --hostname=localhost --port=8001 --open --name='Buddy' &
 # redux-devtools --hostname=localhost --port=8002 --open --name='Dev' &
 # redux-devtools --hostname=localhost --port=8003 --open --name='Mode' &
-
 pnpm run dev
