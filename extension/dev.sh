babb033a1c645716dc515d93e8c58afc1cb39982
Nick Doan <nickbar01234@gmail.com>
2025-06-14 14:05:15 -0400

    Add redux-devtools to docker (#339)
    Co-authored-by: nickbar01234 <nickbar01234gmail.com>

--- extension/dev.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-redux-devtools --hostname=localhost --port=8000 --open --name='Code' &
-redux-devtools --hostname=localhost --port=8001 --open --name='Buddy' &
-# redux-devtools --hostname=localhost --port=8002 --open --name='Dev' &
-# redux-devtools --hostname=localhost --port=8003 --open --name='Mode' &
-
-pnpm run dev
