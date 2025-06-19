bccd0c2af2074abc88276caf57ec573157dbfb3a
Nick Doan <nickbar01234@gmail.com>
2025-06-19 14:43:06 -0400

    Redux window provider (#340)
    * adding redux starter
    
    * update readme instruction
    
    * test apppanel redux
    
    * first pass
    
    * clean up
    
    * merge main
    
    * add event listener
    
    * Revert README
    
    * Revert dev script
    
    * Remove code from merge conflict
    
    * Update pnpm-lock
    
    * Move listener to hook
    
    ---------
    
    Co-authored-by: HUNG NGO <hungngo1607@gmail.com>
    Co-authored-by: Long Dau <dtnlong04@gmail.com>
    Co-authored-by: nickbar01234 <nickbar01234gmail.com>

--- extension/src/components/panel/AppPanel.tsx
+++ extension/src/components/panel/AppPanel.tsx
@@ -1,9 +1,11 @@
 import { CollapsedPanel } from "@cb/components/panel/CollapsedPanel";
 import { VerticalHandle } from "@cb/components/panel/Handle";
-import { MIN_WIDTH } from "@cb/context/WindowProvider";
-import { useWindowDimensions } from "@cb/hooks";
+import { useWindowListener } from "@cb/hooks/listeners";
+import { useWindow } from "@cb/hooks/useWindow";
+import { MIN_WIDTH } from "@cb/state/slices/windowSlice";
 import React from "react";
-import { ResizableBox } from "react-resizable";
+import { ResizableBox, ResizeCallbackData } from "react-resizable";
+
 interface AppPanelProps {
   children?: React.ReactNode;
 }
@@ -12,9 +14,26 @@ export const AppPanel = (props: AppPanelProps) => {
   const {
     preference: { appPreference },
     setAppWidth,
-    onResizeStop,
     toggleWidth,
-  } = useWindowDimensions();
+    onResizeStop,
+  } = useWindow();
+
+  const handleResize = (
+    _e: React.SyntheticEvent<Element, Event>,
+    data: ResizeCallbackData
+  ) => {
+    setAppWidth(data.size.width);
+  };
+
+  const handleResizeStop = (
+    _e: React.SyntheticEvent<Element, Event>,
+    data: ResizeCallbackData
+  ) => {
+    setAppWidth(data.size.width);
+    onResizeStop();
+  };
+
+  useWindowListener();
 
   return (
     <ResizableBox
@@ -24,8 +43,8 @@ export const AppPanel = (props: AppPanelProps) => {
       className="relative flex h-full"
       handle={<div onDoubleClick={toggleWidth}>{VerticalHandle}</div>}
       minConstraints={[MIN_WIDTH, 0]}
-      onResize={(_e, data) => setAppWidth(data.size.width)}
-      onResizeStop={onResizeStop}
+      onResize={handleResize}
+      onResizeStop={handleResizeStop}
     >
       <div className="bg-primary ml-2 box-border h-full w-full rounded-lg">
         {appPreference.isCollapsed && <CollapsedPanel />}
---
+++ extension/src/state/slices/windowSlice.ts
@@ -0,0 +1,79 @@
+// src/store/slices/windowSlice.ts
+import { CodeBuddyPreference } from "@cb/constants";
+import { Preference } from "@cb/types";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+export interface WindowState {
+  width: number;
+  height: number;
+  preference: Preference;
+}
+
+export const MIN_WIDTH = 40;
+
+const getInitialWindowDimensions = () => {
+  const { innerWidth: width, innerHeight: height } = window;
+  return { width, height };
+};
+
+const getInitialPreference = (): Preference => {
+  const stored = localStorage.getItem("preference");
+  return stored ? JSON.parse(stored) : CodeBuddyPreference;
+};
+
+const initialState: WindowState = {
+  ...getInitialWindowDimensions(),
+  preference: getInitialPreference(),
+};
+
+export const windowSlice = createSlice({
+  name: "window",
+  initialState,
+  reducers: {
+    setWindowDimensions: (
+      state,
+      action: PayloadAction<{
+        width: number;
+        height: number;
+        prevW: number;
+        prevH: number;
+      }>
+    ) => {
+      const { width, height, prevW, prevH } = action.payload;
+      const widthRatio = state.preference.appPreference.width / prevW;
+      const heightRatio = state.preference.codePreference.height / prevH;
+
+      state.preference.appPreference.width = width * widthRatio;
+      state.preference.appPreference.isCollapsed =
+        state.preference.appPreference.width <= MIN_WIDTH;
+      state.preference.codePreference.height = height * heightRatio;
+
+      state.width = width;
+      state.height = height;
+    },
+    setAppWidth: (state, action: PayloadAction<number>) => {
+      state.preference.appPreference.width = action.payload;
+      state.preference.appPreference.isCollapsed = action.payload <= MIN_WIDTH;
+    },
+    setCodePreferenceHeight: (state, action: PayloadAction<number>) => {
+      state.preference.codePreference.height = action.payload;
+    },
+    toggleWidth: (state) => {
+      state.preference.appPreference.isCollapsed =
+        !state.preference.appPreference.isCollapsed;
+    },
+    setPreference: (state, action: PayloadAction<Preference>) => {
+      state.preference = action.payload;
+    },
+  },
+});
+
+export const {
+  setWindowDimensions,
+  setAppWidth,
+  setCodePreferenceHeight,
+  toggleWidth,
+  setPreference,
+} = windowSlice.actions;
+
+export default windowSlice.reducer;
