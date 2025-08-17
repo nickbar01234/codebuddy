8d220db1078f168a7ff15220c232ff05699d937c
nickbar01234 <nickbar01234gmail.com>
2025-08-17 01:56:45 -0400

    Paste code

---
+++ extension/src/hooks/editor/index.ts
@@ -0,0 +1,14 @@
+import { usePeers } from "@cb/hooks/store";
+import background from "@cb/services/background";
+import React from "react";
+
+export const usePasteCode = () => {
+  const { selectedPeer } = usePeers();
+  return React.useCallback(
+    () =>
+      background.pasteCode({
+        value: selectedPeer?.code?.value ?? "",
+      }),
+    [selectedPeer]
+  );
+};
