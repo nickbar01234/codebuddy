e69c08ba95ff7b87b9af17c0fbb1c8282e1db1aa
Nick Doan <nickbar01234@gmail.com>
2025-08-29 16:11:47 -0400

    Reset channel (#411)
    * Reset channel
    
    * Toast events on failure
    
    ---------
    
    Co-authored-by: nickbar01234 <nickbar01234gmail.com>

--- extension/src/services/controllers/WebRtcController.ts
+++ extension/src/services/controllers/WebRtcController.ts
@@ -65,6 +65,7 @@ export class WebRtcController {
   }
 
   private connect(user: User) {
+    console.log("Connecting user", user, this.pcs.has(user));
     if (this.pcs.has(user)) {
       return;
     }
@@ -109,6 +110,14 @@ export class WebRtcController {
       }
     };
 
+    pc.oniceconnectionstatechange = () => {
+      if (pc.iceConnectionState === "failed") {
+        console.log("Restarting ICE gathering");
+        // See https://github.com/w3c/webrtc-pc/issues/2167
+        pc.restartIce();
+      }
+    };
+
     const unsubscribeFromIceEvents = this.emitter.on(
       "rtc.ice",
       this.handleIceEvents.bind(this),
@@ -141,6 +150,13 @@ export class WebRtcController {
         console.error("Unable to parse rtc message", error);
       }
     };
+
+    channel.onerror = (error) => {
+      console.error("Error on RTC data channel", error);
+      // todo(nickbar01234): Need to recover when error is thrown. Easiest is to re-do the entire process.
+      this.disconnect(user);
+      this.emitter.emit("rtc.error.connection", { user });
+    };
   }
 
   private async handleDescriptionEvents({
@@ -152,7 +168,6 @@ export class WebRtcController {
 
     const { username: me } = this.appStore.getState().actions.getAuthUser();
 
-    // whether im polite or not
     const polite = this.iamPolite(me, from);
     const pc = connection.pc;
     const readyOffer =
@@ -165,12 +180,9 @@ export class WebRtcController {
     if (connection.ignoreOffer) return;
 
     try {
-      if (offerCollision && polite) {
-        await pc.setLocalDescription({ type: "rollback" });
-      }
-
       connection.isSettingRemoteAnswerPending = data.type === "answer";
       await pc.setRemoteDescription(data);
+      connection.isSettingRemoteAnswerPending = false;
       if (data.type === "offer") {
         await pc.setLocalDescription();
         this.emitter.emit("rtc.description", {
