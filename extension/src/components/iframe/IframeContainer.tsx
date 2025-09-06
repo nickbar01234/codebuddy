9e1ecf60a0a2944252f79dbaea931d2fa41af84e
nickbar01234 <nickbar01234gmail.com>
2025-09-05 21:32:05 -0400

    Reduce states

--- extension/src/components/iframe/IframeContainer.tsx
+++ extension/src/components/iframe/IframeContainer.tsx
@@ -3,18 +3,14 @@ import { useHtml } from "@cb/store/htmlStore";
 import React from "react";
 
 export const IframeContainer: React.FC = () => {
-  const hiddenContainer = useHtml((state) => state.hiddenContainer);
   const htmlElement = useHtml((state) => state.htmlElement);
-
   return (
-    <div ref={hiddenContainer} className="hidden pointer-events-none">
-      <iframe
-        ref={htmlElement}
-        src={URLS.PROBLEMSET}
-        title="LeetCode Question"
-        className="h-full w-full border-2 border-[#78788033]"
-        sandbox="allow-scripts allow-same-origin"
-      />
-    </div>
+    <iframe
+      ref={htmlElement}
+      src={URLS.PROBLEMSET}
+      title="LeetCode Question"
+      className="hidden z-[1000]"
+      sandbox="allow-scripts allow-same-origin"
+    />
   );
 };
