c4b7cb760ebe60b277463d16d631357f2b716038
PhuongLe <phuongle@PhuongLes-MacBook-Pro.local>
2025-09-07 19:22:14 -0400

    nothing done yet, still just playing around

--- extension/src/entrypoints/content.tsx
+++ extension/src/entrypoints/content.tsx
@@ -13,6 +13,7 @@ export default defineContentScript({
     // Initialize controllers on startup
     getOrCreateControllers();
     await injectScript("/proxy.js", { keepInDom: true });
+    injectTestCaseMinusOne();
     const ui = createUi(ctx);
     ui.mount();
   },
@@ -39,3 +40,43 @@ const createUi = (ctx: ContentScriptContext) => {
     onMount: () => {},
   });
 };
+
+function injectTestCaseMinusOne() {
+  // const TEST_CASE_TABBER_ID = 'testcase_tabbar_outer';
+  // const testCaseParent = document.getElementById(TEST_CASE_TABBER_ID);
+  // if (!testCaseParent) {
+  //   console.warn('Test case parent node not found');
+  //   return;
+  // }
+  // console.log('testCaseParent:', testCaseParent);
+
+  const allTestCaseButtons = document.querySelectorAll(
+    '[data-e2e-locator="console-testcase-tag"]'
+  );
+  if (!allTestCaseButtons) {
+    console.warn("Test case button to clone not found");
+    return;
+  }
+
+  const testCaseButton = allTestCaseButtons[allTestCaseButtons.length - 1];
+
+  const testCaseParent = testCaseButton.parentElement;
+  if (!testCaseParent) {
+    console.warn("Test case button parent not found");
+    return;
+  }
+
+  // Clone the button and update its label
+  const newButton = testCaseButton.cloneNode(true);
+  if (newButton instanceof HTMLElement) {
+    newButton.textContent = "Case -1";
+    newButton.classList.add("testcase-minus-one");
+    newButton.addEventListener("click", (event) => {
+      // Your logic here
+      console.log("Case -1 button clicked");
+      newButton.className =
+        "font-big items-center whitespace-nowrap focus:outline-none inline-flex bg-fill-3 dark:bg-dark-fill-3 hover:bg-fill-2 dark:hover:bg-dark-fill-2 relative rounded-lg px-4 py-1 hover:text-label-1 dark:hover:text-dark-label-1 text-label-1 dark:text-dark-label-1";
+    });
+    testCaseButton.insertAdjacentElement("afterend", newButton);
+  }
+}
