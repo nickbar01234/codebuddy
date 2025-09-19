7803bea8b1addc2e3a26fa9e222adfc1d71207fd
Nick Doan <nickbar01234@gmail.com>
2025-09-18 23:22:58 -0400

    Add room metadata (#443)
    * Add room metadata
    
    * Fix test code
    
    * Add padding
    
    ---------
    
    Co-authored-by: nickbar01234 <nickbar01234gmail.com>

--- extension/src/components/panel/info/GeneralRoomInfo.tsx
+++ extension/src/components/panel/info/GeneralRoomInfo.tsx
@@ -2,18 +2,52 @@ import { DefaultTable } from "@cb/components/table/DefaultTable";
 import { DefaultTableBody } from "@cb/components/table/DefaultTableBody";
 import { DefaultTableHeader } from "@cb/components/table/DefaultTableHeader";
 import { DefaultTableRow } from "@cb/components/table/DefaultTableRow";
-import { useAuthUser, usePeers } from "@cb/hooks/store";
+import { SkeletonWrapper } from "@cb/components/ui/SkeletonWrapper";
+import { ROOM } from "@cb/constants";
+import { useAuthUser, usePeers, useRoomData } from "@cb/hooks/store";
 import { TableCell } from "@cb/lib/components/ui/table";
-import { Info } from "lucide-react";
+import { CopyIcon, Globe, Info, Users } from "lucide-react";
 import { BaseInfoSheet } from "./BaseInfoSheet";
 
 export const GeneralRoomInfo = () => {
+  const { name, id } = useRoomData();
   const { peers } = usePeers();
   const { username } = useAuthUser();
+  const copyRoomId = useCopyRoomId();
   const users = [...Object.keys(peers), username];
 
   return (
     <BaseInfoSheet trigger={<Info />}>
+      <div className="flex flex-col gap-4 mb-8">
+        <SkeletonWrapper loading={name == undefined} className="h-10 w-48">
+          <div className="flex gap-2 items-center text-secondary">
+            <h2 className="text-2xl">
+              {name && name.length > 0 ? name : "Room Information"}
+            </h2>
+            <div className="rounded-md bg-quaternary p-2 text-center self-center">
+              <Globe />
+            </div>
+          </div>
+        </SkeletonWrapper>
+        <SkeletonWrapper loading={id == undefined} className="w-60 h-8">
+          <div className="flex gap-1 text-tertiary">
+            <span className="text-lg">ID: {id}</span>
+            <CopyIcon
+              className="cursor-pointer hover:bg-[--color-button-hover-background] dark:hover:bg-[--color-button-hover-background] p-1 hover:rounded-md self-center"
+              onClick={copyRoomId}
+            />
+          </div>
+        </SkeletonWrapper>
+      </div>
+      <div className="mb-2 flex gap-3 items-center text-secondary">
+        <div className="text-xl font-semibold">Members</div>
+        <div className="flex items-center rounded-md bg-quaternary p-2 text-sm gap-2">
+          <Users />
+          <span>
+            {users.length} / {ROOM.CAPACITY}
+          </span>
+        </div>
+      </div>
       <DefaultTable loading={users.length === 0}>
         <DefaultTableHeader headers={["Rank", "User", "Problem solved"]} />
         <DefaultTableBody>
