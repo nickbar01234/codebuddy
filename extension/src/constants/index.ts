7b8bca1099d0c34f34c5ca70223216fff5504c6e
nickbar01234 <nickbar01234gmail.com>
2025-09-20 06:15:36 -0400

    Disable feature flag

--- extension/src/constants/index.ts
+++ extension/src/constants/index.ts
@@ -65,7 +65,7 @@ export const WEB_RTC_ICE_SERVERS: Record<"STUN" | "TURN", RTCIceServer[]> = {
 };
 
 export const FEATURE_FLAG = {
-  DISABLE_MULTI_URLS: false,
+  DISABLE_MULTI_URLS: true,
 };
 
 export const CSS = {
