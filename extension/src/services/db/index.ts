10300c0dd382777523f6ff23f89b57e9f37e288e
nickbar01234 <nickbar01234gmail.com>
2025-10-20 00:06:18 -0400

    Paginate and show data

--- extension/src/services/db/index.ts
+++ extension/src/services/db/index.ts
@@ -1,6 +1,15 @@
+import { Models } from "@cb/types";
+import { collection, query, where } from "firebase/firestore";
 import { auth, firebaseDatabaseServiceImpl } from "./firebase";
+import { roomConverter } from "./firebase/converter";
+import { firestore } from "./firebase/setup";
 
 const db = firebaseDatabaseServiceImpl;
+
+export const roomQuery = query(
+  collection(firestore, Models.ROOMS).withConverter(roomConverter),
+  where("isPublic", "==", true)
+);
 
 export default db;
 export { auth };
