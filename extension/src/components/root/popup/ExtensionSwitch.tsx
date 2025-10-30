989005e04334e2c772dccd524453d15914456b41
Nick Doan <nickbar01234@gmail.com>
2025-10-30 00:00:21 -0400

    Add wxt popup (#386)
    * Add wxt popup
    
    * Add wxt popup
    
    * popup Extension Switch
    
    * small fix
    
    * resolve conflict
    
    * clean up
    
    * small fix
    
    * add type
    
    * Fix lock
    
    * clean up
    
    ---------
    
    Co-authored-by: nickbar01234 <nickbar01234gmail.com>
    Co-authored-by: lnguyen2693 <n9linh@gmail.com>

---
+++ extension/src/components/root/popup/ExtensionSwitch.tsx
@@ -0,0 +1,31 @@
+import { URLS } from "@cb/constants";
+import { Switch } from "@cb/lib/components/ui/Switch";
+import { useApp } from "@cb/store/appStore";
+import React from "react";
+
+export const ExtensionSwitch = () => {
+  const enabled = useApp((state) => state.app.enabled);
+  const toggleEnabledAppInternal = useApp(
+    (state) => state.actions.toggleEnabledApp
+  );
+
+  const toggleEnabledApp = React.useCallback(() => {
+    toggleEnabledAppInternal();
+    browser.tabs.query({ url: URLS.ALL_PROBLEMS }).then((tabs) =>
+      tabs.forEach((tab) => {
+        if (tab.id != undefined) {
+          browser.tabs.sendMessage(tab.id, { action: "toggleUi" });
+        }
+      })
+    );
+  }, [toggleEnabledAppInternal]);
+
+  return (
+    <div className="flex justify-between px-5 py-3">
+      <div>Enable extension</div>
+      <div>
+        <Switch checked={enabled} onCheckedChange={toggleEnabledApp} />
+      </div>
+    </div>
+  );
+};
