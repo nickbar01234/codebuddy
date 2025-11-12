e3b103473f597a035b77b8c800a30c75ebe82942
nickbar01234 <nickbar01234gmail.com>
2025-11-12 10:34:24 -0500

    Bump to 3.6.3

--- extension/wxt.config.ts
+++ extension/wxt.config.ts
@@ -22,7 +22,7 @@ export default defineConfig({
   manifest: {
     name: "CodeBuddy",
     description: "Leetcode together",
-    version: "3.6.2",
+    version: "3.6.3",
     action: {},
     icons: {
       "16": "icons/16.png",
