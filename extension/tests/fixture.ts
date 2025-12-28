204d5b4e737fdda0fdd1b4b80ea6df009a16b6d0
nickbar01234 <nickbar01234gmail.com>
2025-12-28 12:37:21 -0500

    Add DOM loaded test

---
+++ extension/tests/fixture.ts
@@ -0,0 +1,42 @@
+import { test as base, chromium, type BrowserContext } from "@playwright/test";
+import { dirname, resolve } from "path";
+import { fileURLToPath } from "url";
+
+const extension = resolve(
+  dirname(fileURLToPath(import.meta.url)),
+  "../dist/chrome-mv3"
+);
+
+export const test = base.extend<{
+  context: BrowserContext;
+  extensionId: string;
+}>({
+  // eslint-disable-next-line no-empty-pattern
+  context: async ({}, use) => {
+    const context = await chromium.launchPersistentContext("", {
+      headless: false,
+      args: [
+        `--disable-extensions-except=${extension}`,
+        `--load-extension=${extension}`,
+      ],
+    });
+    await use(context);
+    await context.close();
+  },
+  extensionId: async ({ context }, use) => {
+    let [serviceWorker] = context.serviceWorkers();
+    if (!serviceWorker)
+      serviceWorker = await context.waitForEvent("serviceworker");
+
+    const extensionId = serviceWorker.url().split("/")[2];
+    await use(extensionId);
+  },
+  page: async ({ page }, use) => {
+    await page.goto("https://leetcode.com/problems/two-sum", {
+      waitUntil: "domcontentloaded",
+    });
+    use(page);
+  },
+});
+
+export const expect = test.expect;
