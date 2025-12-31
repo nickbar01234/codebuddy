80300486a11b6fa0c606abb70762f11999f64b2a
Nick Doan <nickbar01234@gmail.com>
2025-12-30 22:49:03 -0500

    Bump to 3.11.0 (#582)
    Co-authored-by: nickbar01234 <nickbar01234gmail.com>

--- extension/wxt.config.ts
+++ extension/wxt.config.ts
@@ -38,7 +38,7 @@ export default defineConfig({
   manifest: {
     name: "CodeBuddy",
     description: "Leetcode together",
-    version: "3.10.0",
+    version: "3.11.0",
     action: {},
     icons: {
       "16": "icons/16.png",
